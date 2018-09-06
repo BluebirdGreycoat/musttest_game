f0b72e69de41a9bcf39a3f52778e4f76ec28ff00
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2018-09-06 18:27:00 -0500

    Delete gems mod, in preparation for a replacement.
    The gems mod is licensed under the GPLv3. It is therefore not legally
    compatible with the licensing goals of this game.

--- mods/gems/init.lua
+++
@@ -1,8 +0,0 @@
-
-gems = gems or {}
-gems.modpath = minetest.get_modpath("gems")
-
-dofile(gems.modpath .. "/items.lua")
-dofile(gems.modpath .. "/tools.lua")
-dofile(gems.modpath .. "/crafts.lua")
-dofile(gems.modpath .. "/oregen.lua")

fb1e767a4316115fbc766ad32fe3ec17ff72bf91
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-01-31 13:12:03 -0600

    Update

--- mods/cw/init.lua
+++ mods/cw/init.lua
@@ -8,11 +8,18 @@ cw.modpath = minetest.get_modpath("cw")
 
 -- Register deadly water.
 if not cw.registered then
+	-- Basically just a copy of regular water, with damage_per_second.
 	local sdef = table.copy(minetest.registered_nodes["default:water_source"])
 	local fdef = table.copy(minetest.registered_nodes["default:water_flowing"])
 
-	sdef.damage_per_second = 2
-	fdef.damage_per_second = 2
+	sdef.damage_per_second = 1
+	fdef.damage_per_second = 1
+
+	sdef.liquid_alternative_flowing = "cw:water_flowing",
+	sdef.liquid_alternative_source = "cw:water_source",
+
+	fdef.liquid_alternative_flowing = "cw:water_flowing",
+	fdef.liquid_alternative_source = "cw:water_source",
 
 	minetest.register_node("cw:water_source", sdef)
 	minetest.register_node("cw:water_flowing", fdef)

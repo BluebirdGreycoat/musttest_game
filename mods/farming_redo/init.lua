ed50e067929b68bff7ceaa7bf14f69cf6e8ffdb5
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-02-13 14:57:07 -0600

    Update farming stuff, add more stuff

---
+++ mods/farming_redo/init.lua
@@ -0,0 +1,202 @@
+
+minetest.register_craftitem(":farming:bread_slice", {
+	description = "Sliced Bread",
+	inventory_image = "farming_bread_slice.png",
+	on_use = minetest.item_eat(1),
+	groups = {food_bread_slice = 1, flammable = 2},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "farming:bread_slice 5",
+	recipe = {"farming:bread", "farming:cutting_board"},
+	replacements = {{"farming:cutting_board", "farming:cutting_board"}},
+})
+
+-- mortar & pestle uses only 3 wheat to make flour, contast with hand-craft which uses 4 wheat for 1 flour
+minetest.register_craft({
+	type = "shapeless",
+	output = "farming:flour",
+	recipe = {
+		"farming:wheat", "farming:wheat", "farming:wheat",
+		"farming:mortar_pestle"
+	},
+	replacements = {{"farming:mortar_pestle", "farming:mortar_pestle"}},
+})
+
+minetest.register_craftitem(":farming:toast", {
+	description = "Toast",
+	inventory_image = "farming_toast.png",
+	on_use = minetest.item_eat(1),
+	groups = {food_toast = 1, flammable = 2},
+})
+
+minetest.register_craftitem(":farming:toast_sandwich", {
+	description = "Toast Sandwich",
+	inventory_image = "farming_toast_sandwich.png",
+	on_use = minetest.item_eat(4),
+	groups = {flammable = 2},
+})
+
+minetest.register_craft({
+	output = "farming:toast_sandwich",
+	recipe = {
+		{"farming:bread_slice"},
+		{"farming:toast"},
+		{"farming:bread_slice"},
+	}
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "farming:straw",
+	burntime = 3,
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "farming:straw_weathered",
+	burntime = 3,
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "darkage:straw_bale",
+	burntime = 3*9,
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "farming:wheat",
+	burntime = 1,
+})
+
+minetest.register_craft({
+	type = "cooking",
+	cooktime = 3,
+	output = "farming:toast",
+	recipe = "farming:bread_slice"
+})
+
+minetest.register_craftitem(":farming:cutting_board", {
+	description = "Cutting Board",
+	inventory_image = "farming_cutting_board.png",
+	groups = {food_cutting_board = 1, flammable = 2},
+})
+
+minetest.register_craft({
+	output = "farming:cutting_board",
+	recipe = {
+		{"default:steel_ingot", "", ""},
+		{"", "group:stick", ""},
+		{"", "", "group:wood"},
+	}
+})
+
+minetest.register_craftitem(":farming:saucepan", {
+	description = "Saucepan",
+	inventory_image = "farming_saucepan.png",
+	groups = {food_saucepan = 1, flammable = 2},
+})
+
+minetest.register_craft({
+	output = "farming:saucepan",
+	recipe = {
+		{"default:steel_ingot", "", ""},
+		{"", "group:stick", ""},
+	}
+})
+
+minetest.register_craftitem(":farming:pot", {
+	description = "Cooking Pot",
+	inventory_image = "farming_pot.png",
+	groups = {food_pot = 1, flammable = 2},
+})
+
+minetest.register_craft({
+	output = "farming:pot",
+	recipe = {
+		{"group:stick", "default:steel_ingot", "default:steel_ingot"},
+		{"", "default:steel_ingot", "default:steel_ingot"},
+	}
+})
+
+minetest.register_craftitem(":farming:baking_tray", {
+	description = "Baking Tray",
+	inventory_image = "farming_baking_tray.png",
+	groups = {food_baking_tray = 1, flammable = 2},
+})
+
+minetest.register_craft({
+	output = "farming:baking_tray",
+	recipe = {
+		{"default:clay_brick", "default:clay_brick", "default:clay_brick"},
+		{"default:clay_brick", "", "default:clay_brick"},
+		{"default:clay_brick", "default:clay_brick", "default:clay_brick"},
+	}
+})
+
+minetest.register_craftitem(":farming:skillet", {
+	description = "Skillet",
+	inventory_image = "farming_skillet.png",
+	groups = {food_skillet = 1, flammable = 2},
+})
+
+minetest.register_craft({
+	output = "farming:skillet",
+	recipe = {
+		{"default:steel_ingot", "", ""},
+		{"", "default:steel_ingot", ""},
+		{"", "", "group:stick"},
+	}
+})
+
+minetest.register_craftitem(":farming:mortar_pestle", {
+	description = "Mortar and Pestle",
+	inventory_image = "farming_mortar_pestle.png",
+	groups = {food_mortar_pestle = 1, flammable = 2},
+})
+
+minetest.register_craft({
+	output = "farming:mortar_pestle",
+	recipe = {
+		{"default:stone", "group:stick", "default:stone"},
+		{"", "default:stone", ""},
+	}
+})
+
+minetest.register_craftitem(":farming:juicer", {
+	description = "Juicer",
+	inventory_image = "farming_juicer.png",
+	groups = {food_juicer = 1, flammable = 2},
+})
+
+minetest.register_craft({
+	output = "farming:juicer",
+	recipe = {
+		{"", "default:stone", ""},
+		{"default:stone", "", "default:stone"},
+	}
+})
+
+minetest.register_craftitem(":farming:mixing_bowl", {
+	description = "Glass Mixing Bowl",
+	inventory_image = "farming_mixing_bowl.png",
+	groups = {food_mixing_bowl = 1, flammable = 2},
+})
+
+minetest.register_craft({
+	output = "farming:mixing_bowl",
+	recipe = {
+		{"default:glass", "group:stick", "default:glass"},
+		{"", "default:glass", ""},
+	}
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "vessels:glass_fragments",
+	recipe = {
+		"farming:mixing_bowl",
+	},
+})

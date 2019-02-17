04687955855d22348e5c113387440e58f26b6653
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-02-16 22:49:53 -0600

    Fix arrows

--- mods/throwing/functions.lua
+++ mods/throwing/functions.lua
@@ -2,17 +2,17 @@
 function throwing.entity_blocks_arrow(entity_name)
 	-- Dropped itemstacks don't take damage.
 	if entity_name == "__builtin:item" then
-		return false
+		return true
 	end
 
 	-- Ignore other arrows/fireballs in flight.
 	local is_arrow = (string.find(entity_name, "arrow") or string.find(entity_name, "fireball"))
 	if is_arrow then
-		return false
+		return true
 	end
 
 	-- Entity is unknown, so punch it for damage!
-	return true
+	return false
 end
 
 
--- mods/throwing/newfunc.lua
+++ mods/throwing/newfunc.lua
@@ -1,30 +1,15 @@
--- function only loaded on specific dofile command from game console.
-function throwing_arrow_punch_entity (obj, self, damage)
-  local player = minetest.get_player_by_name(self.player_name or "")
-  if player and player:is_player() then
-		if obj:is_player() then
-			-- If target is a player, and not a mob, we can't use the shooter as the
-			-- attacker. This would only actually apply damage if the shooter was a
-			-- short distance from the target. So for this case, we have to "fake" the
-			-- target punching themselves.
-			obj:punch(self.object, 1.0, {
-				full_punch_interval=1.0,
-				damage_groups={fleshy=damage},
-			}, nil)
-		else
-			-- The target of the arrow (a mob) sees the shooter as the attacker,
-			-- *not* the arrow entity itself. If this were not so, players
-			-- could shoot mobs with arrows without retaliation.
-			obj:punch(player, 1.0, {
-				full_punch_interval=1.0,
-				damage_groups={fleshy=damage},
-			}, nil)
-		end
-  else
-		-- Shooter logged off game after firing arrow. Use basic fallback.
-    obj:punch(self.object, 1.0, {
-      full_punch_interval=1.0,
-      damage_groups={fleshy=damage},
-    }, nil)
-  end
+function throwing.entity_blocks_arrow(entity_name)
+	-- Dropped itemstacks don't take damage.
+	if entity_name == "__builtin:item" then
+		return true
+	end
+
+	-- Ignore other arrows/fireballs in flight.
+	local is_arrow = (string.find(entity_name, "arrow") or string.find(entity_name, "fireball"))
+	if is_arrow then
+		return true
+	end
+
+	-- Entity is unknown, so punch it for damage!
+	return false
 end

d9af50d741aa21ac2b49bcd3d77960f991143224
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-02-17 15:33:28 -0600

    Remove lagger

--- mods/mob_spawn/init.lua
+++ mods/mob_spawn/init.lua
@@ -257,17 +257,13 @@ local function search_terrain(pos, step, radius, jitter, nodes, offset, height)
 		gp.y = y + random(-jitter, jitter)
 		gp.z = z + random(-jitter, jitter)
 
-		local success, np = try_locate_ground(gp)
+		local bw = get_node(np).name
 
-		if success then
-			local bw = get_node(np).name
-
-			for i = 1, #nodes do
-				if bw == nodes[i] then
-					if check_space(vector.add(np, {x=0, y=offset, z=0}), vector.add(np, {x=0, y=offset+(height-1), z=0}), bw) then
-						results[#results+1] = {x=np.x, y=np.y+offset, z=np.z}
-						break
-					end
+		for i = 1, #nodes do
+			if bw == nodes[i] then
+				if check_space(vector.add(gp, {x=0, y=offset, z=0}), vector.add(gp, {x=0, y=offset+(height-1), z=0}), bw) then
+					results[#results+1] = {x=gp.x, y=gp.y+offset, z=gp.z}
+					break
 				end
 			end
 		end

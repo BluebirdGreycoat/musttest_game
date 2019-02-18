24e64119ffd72127cec9dfe9c43e4ea91633df11
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-02-17 22:14:16 -0600

    Update dumpnodes mod

--- mods/dumpnodes/init.lua
+++ mods/dumpnodes/init.lua
@@ -32,72 +32,83 @@ end
 
 
 
+-- Name and param may be empty strings.
 dumpnodes.execute = function(plname, param)
-    minetest.chat_send_player(plname, "# Server: Preparing to dump nodes.")
-    
-    local n = 0
-    local ntbl = {}
-    
-    for _, nn in pairs_s(minetest.registered_nodes) do
-        local nd = minetest.registered_nodes[nn]
-        local prefix, name = nn:match('(.*):(.*)')
-        if prefix == nil or name == nil or prefix == '' or name == '' then
-            print("ignored(1): " .. nn)
-        else
-            if ntbl[prefix] == nil then
-                ntbl[prefix] = {}
-            end
-            ntbl[prefix][name] = nd
-        end
-    end
-    
-    local out = io.open(minetest.get_worldpath() .. '/dumpnodes.txt', 'wb')
-    if not out then
-        minetest.chat_send_player(plname, "# Server: Could not open dumpnodes file.")
-				easyvend.sound_error(plname)
-        return true
-    end
-    
-    for _, prefix in pairs_s(ntbl) do
-        local nodes = ntbl[prefix]
-        out:write('# ' .. prefix .. '\n')
-        for _, name in pairs_s(nodes) do
-            local nd = nodes[name]
-            if nd.drawtype ~= 'airlike' and nd_get_tiles(nd) ~= nil then
-                local tl = nd_get_tiles(nd)[1]
-                if type(tl) == 'table' then
-                    tl = tl.name
-                end
-                tl = (tl .. '^'):match('(.-)^')
-                out:write(prefix .. ':' .. name .. ' ' .. tl .. '\n')
-                n = n + 1
-            else
-                print("ignored(2): " .. prefix .. ':' .. name)
-            end
-        end
-        out:write('\n')
-    end
-    
-    out:close()
-    minetest.chat_send_player(plname, "# Server: " .. n .. " nodes dumped.")
-    return true
+	local n = 0
+	local ntbl = {}
+
+	for _, nn in pairs_s(minetest.registered_nodes) do
+		local nd = minetest.registered_nodes[nn]
+		local prefix, name = nn:match('(.*):(.*)')
+		if prefix == nil or name == nil or prefix == '' or name == '' then
+			print("ignored(1): " .. nn)
+		else
+			if ntbl[prefix] == nil then
+				ntbl[prefix] = {}
+			end
+			ntbl[prefix][name] = nd
+		end
+	end
+
+	local out = io.open(minetest.get_worldpath() .. '/dumpnodes.txt', 'wb')
+	if not out then
+		return false, "Could not open dumpnodes file."
+	end
+
+	for _, prefix in pairs_s(ntbl) do
+		local nodes = ntbl[prefix]
+		out:write('# ' .. prefix .. '\n')
+		for _, name in pairs_s(nodes) do
+			local nd = nodes[name]
+			if nd.drawtype ~= 'airlike' and nd_get_tiles(nd) ~= nil then
+				local tl = nd_get_tiles(nd)[1]
+				if type(tl) == 'table' then
+					tl = tl.name
+				end
+				tl = (tl .. '^'):match('(.-)^')
+				out:write(prefix .. ':' .. name .. ' ' .. tl .. '\n')
+				n = n + 1
+			else
+				print("ignored(2): " .. prefix .. ':' .. name)
+			end
+		end
+		out:write('\n')
+	end
+
+	out:close()
+	return true, "" .. n .. " nodes dumped."
+end
+
+dumpnodes.chatcommand = function(pname, param)
+	minetest.chat_send_player(pname, "# Server: Preparing to dump nodes.")
+	local result, msg = dumpnodes.execute(pname, param)
+	if msg then
+		minetest.chat_send_player(pname, "# Server: " .. msg)
+	end
+	if not result then
+		easyvend.sound_error(pname)
+	end
 end
 
 
 
 if not dumpnodes.run_once then
-    minetest.register_chatcommand("dumpnodes", {
-        params = "",
-        description = "",
-        privs = {server=true},
-        func = function(...) return dumpnodes.execute(...) end,
-    })
-    
-    local name = "dumpnodes:core"
-    local file = dumpnodes.modpath .. "/init.lua"
-    reload.register_file(name, file, false)
-    
-    dumpnodes.run_once = true
+	minetest.register_chatcommand("dumpnodes", {
+		params = "",
+		description = "",
+		privs = {server=true},
+		func = function(...) return dumpnodes.chatcommand(...) end,
+	})
+
+	minetest.register_on_shutdown(function()
+		dumpnodes.execute("", "")
+	end)
+
+	local name = "dumpnodes:core"
+	local file = dumpnodes.modpath .. "/init.lua"
+	reload.register_file(name, file, false)
+
+	dumpnodes.run_once = true
 end
 
 

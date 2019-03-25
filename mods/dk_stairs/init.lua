bffbf30ae5ad9da78f19a9da776aa117bd808783
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-03-25 12:56:21 -0500

    Add missing darkage stuff to walls/castles registration

--- mods/dk_stairs/init.lua
+++ mods/dk_stairs/init.lua
@@ -14,21 +14,30 @@ local register_stairs = function(basename)
 	end
 end
 
+
 register_stairs("basaltic")
 register_stairs("basaltic_brick")
 register_stairs("basaltic_rubble")
+
 register_stairs("chalked_bricks")
+
 register_stairs("gneiss")
 register_stairs("gneiss_brick")
 register_stairs("gneiss_rubble")
+
 register_stairs("marble")
-register_stairs("marble_tile")
+
+register_stairs("tuff")
+register_stairs("tuff_bricks")
 register_stairs("old_tuff_bricks")
+
 register_stairs("ors")
 register_stairs("ors_brick")
 register_stairs("ors_rubble")
+
 register_stairs("rhyolitic_tuff")
 register_stairs("rhyolitic_tuff_bricks")
+
 register_stairs("schist")
 register_stairs("shale")
 register_stairs("slate")
@@ -36,7 +45,8 @@ register_stairs("slate_brick")
 register_stairs("slate_rubble")
 register_stairs("slate_tile")
 register_stairs("stone_brick")
+
+-- Special: stairs ONLY, no walls/castle stuff!
+register_stairs("marble_tile")
 register_stairs("straw_bale")
-register_stairs("tuff")
-register_stairs("tuff_bricks")
 
--- mods/dk_walls/init.lua
+++ mods/dk_walls/init.lua
@@ -16,11 +16,34 @@ local register_wall = function(basename)
 	end	
 end
 
+register_wall("basaltic")
+register_wall("basaltic_brick")
 register_wall("basaltic_rubble")
-register_wall("ors_rubble")
-register_wall("stone_brick")
-register_wall("slate_rubble")
+
+register_wall("chalked_bricks")
+
+register_wall("gneiss")
+register_wall("gneiss_brick")
+register_wall("gneiss_rubble")
+
+register_wall("marble")
+
+register_wall("tuff")
 register_wall("tuff_bricks")
 register_wall("old_tuff_bricks")
+
+register_wall("ors")
+register_wall("ors_brick")
+register_wall("ors_rubble")
+
+register_wall("rhyolitic_tuff")
 register_wall("rhyolitic_tuff_bricks")
+
+register_wall("schist")
+register_wall("shale")
+register_wall("slate")
+register_wall("slate_brick")
+register_wall("slate_rubble")
+register_wall("slate_tile")
+register_wall("stone_brick")
 

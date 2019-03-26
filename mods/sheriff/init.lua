34e35ebf7584eb8a9590194240b454d1eed2f4bf
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-03-26 13:44:56 -0500

    Add punishment API query function

--- mods/sheriff/init.lua
+++ mods/sheriff/init.lua
@@ -2,7 +2,16 @@
 sheriff = sheriff or {}
 sheriff.modpath = minetest.get_modpath("sheriff")
 
+local players = {
+	"MustTest",
+}
 
+-- Let other mods query whether a give player is being punished.
+function sheriff.player_punished(pname)
+	if players[pname] then
+		return true
+	end
+end
 
 if not sheriff.loaded then
 	-- Register reloadable mod.

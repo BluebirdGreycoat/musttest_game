c6cd599b6b29704cb251af750bf0e8d7e06f3128
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-08-10 15:37:20 +0000

    Remove unneeded ABM

--- mods/3d_armor_stand/init.lua
+++ mods/3d_armor_stand/init.lua
@@ -322,6 +322,7 @@ minetest.register_entity("3d_armor_stand:armor_entity", {
 	end,
 })
 
+--[[
 minetest.register_abm({
 	nodenames = {"3d_armor_stand:locked_armor_stand", "3d_armor_stand:armor_stand"},
 	interval = 15,
@@ -333,6 +334,7 @@ minetest.register_abm({
 		update_entity(pos)
 	end
 })
+--]]
 
 minetest.register_craft({
 	output = "3d_armor_stand:armor_stand",

a3e2fe9b67ead653341b56a74bcc708d77be63f4
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-08-16 19:57:13 -0500

    Set currency values

--- mods/currency/init.lua
+++ mods/currency/init.lua
@@ -4,6 +4,18 @@ currency.modpath = minetest.get_modpath("currency")
 
 
 
+local currency_names = {
+	"currency:minegeld",
+	"currency:minegeld_5",
+	"currency:minegeld_10",
+	"currency:minegeld_50",
+	"currency:minegeld_100",
+}
+
+local currency_values = {1, 5, 10, 50, 100}
+
+
+
 function currency.add_cash(inv, name, amount)
 end
 

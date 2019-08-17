4b31a27e55bffca304f4dabe2910e7b573ec2e90
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-08-16 19:46:52 -0500

    Make currency mod reloadable

--- mods/currency/init.lua
+++ mods/currency/init.lua
@@ -1,4 +1,37 @@
-local modpath = minetest.get_modpath("currency")
 
-dofile(modpath .. "/craftitems.lua")
-dofile(modpath .. "/crafting.lua")
+currency = currency or {}
+currency.modpath = minetest.get_modpath("currency")
+
+
+
+function currency.add_cash(inv, name, amount)
+end
+
+
+
+function currency.del_cash(inv, name, amount)
+end
+
+
+
+function currency.has_cash(inv, name, amount)
+	return (currency.get_cash(inv, name) >= amount)
+end
+
+
+
+function currency.get_cash(inv, name)
+end
+
+
+
+if not currency.registered then
+	--dofile(currency.modpath .. "/craftitems.lua")
+	--dofile(currency.modpath .. "/crafting.lua")
+
+	local c = "currency:core"
+	local f = currency.modpath .. "/init.lua"
+	reload.register_file(c, f, false)
+
+	currency.registered = true
+end

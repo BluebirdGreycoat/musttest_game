77ffbe9ec06658b1dc741eafc01222bd5e9496b4
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-08-17 14:22:25 -0500

    Add polish

--- mods/easyvend/easyvend.lua
+++ mods/easyvend/easyvend.lua
@@ -513,7 +513,7 @@ easyvend.make_infotext = function(pos, nodename, owner, cost, number, itemstring
     local meta = minetest.get_meta(pos)
     local machine_currency = meta:get_string("machine_currency")
     
-    local machine_currency_desc = minetest.registered_items[machine_currency].description
+    local machine_currency_desc = utility.get_short_desc(minetest.registered_items[machine_currency].description)
     if machine_currency_desc == nil or machine_currency_desc == "" then
         machine_currency_desc = machine_currency
     end

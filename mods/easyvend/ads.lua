1d56311aedda7e78e5fbdeb04b4b11d20a256f03
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-08-20 13:47:04 +0000

    Update buttons

--- mods/easyvend/ads.lua
+++ mods/easyvend/ads.lua
@@ -446,8 +446,8 @@ function ads.generate_formspec(pos, pname, booth)
 
 			-- Edit/remove add buttons.
 			formspec = formspec ..
-				"button[0,8.5;2,1;editrecord;" .. minetest.formspec_escape("Edit Record") .. "]" ..
-				"button[2,8.5;3,1;deleterecord;" .. minetest.formspec_escape("Delete Record For Refund") .. "]"
+				"button[0,8.4;2,1;editrecord;" .. minetest.formspec_escape("Edit Record") .. "]" ..
+				"button[2,8.4;4,1;deleterecord;" .. minetest.formspec_escape("Delete Record For Refund") .. "]"
 		end
 	end
 

6530a153cedf4801d4e58e1542610aa5a27d405f
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-08-26 18:42:55 -0500

    Fix a bug

--- mods/engraver/init.lua
+++ mods/engraver/init.lua
@@ -169,9 +169,6 @@ local function handle_engraver_use(player, formname, fields)
 		return true
 	end
 
-	-- Translate escape sequences.
-	message = string.gsub(message, "%%[nN]", "\n")
-
 	-- Add wear to the chisel.
 	local got_chisel = false
 	local inv = player:get_inventory()
@@ -194,6 +191,10 @@ local function handle_engraver_use(player, formname, fields)
 		meta:set_string("chiseled_date", os.time())
 		meta:set_int("engraver_chiseled", 1)
 		meta:mark_as_private({"chiseled_text", "chiseled_date", "engraver_chiseled"})
+
+		-- Translate escape sequences.
+		message = string.gsub(message, "%%[nN]", "\n")
+
 		meta:set_string("infotext", "Chiseled Text Reads:\n\n" .. message)
 		ambiance.sound_play("anvil_clang", pos, 1.0, 30)
 	end

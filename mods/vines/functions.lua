e37b8967907f80674c88ef319d57669b67e09ffb
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-08-26 12:05:53 -0500

    Fix more whitespace issues

--- mods/vines/functions.lua
+++ mods/vines/functions.lua
@@ -11,18 +11,18 @@ vines.destroy_rope_starting = function(p, targetnode, bottomnode, topnode)
 	local tab = {}
 	local i = 1
 	while n == targetnode do
-        --print("test2")
+		--print("test2")
 		tab[i] = p
 		i = i+1
 		p.y = p.y-1
 		n = minetest.get_node(p).name
 	end
-    --print("test10")
+	--print("test10")
 	if n == bottomnode then
 		tab[i] = p
 	end
 	local y0 = p.y
-    --print("test5")
+	--print("test5")
 	local manip = minetest.get_voxel_manip()
 	local p0 = {x=p.x, y=y0,   z=p.z}
 	local p1 = {x=p.x, y=y0+1, z=p.z}
@@ -30,20 +30,20 @@ vines.destroy_rope_starting = function(p, targetnode, bottomnode, topnode)
 	local pos1, pos2 = manip:read_from_map(p0, p2)
 	local area = VoxelArea:new({MinEdge=pos1, MaxEdge=pos2})
 	local nodes = manip:get_data()
-    --print("test6")
-    --print(minetest.pos_to_string(p1) .. ", " .. minetest.pos_to_string(p2))
+	--print("test6")
+	--print(minetest.pos_to_string(p1) .. ", " .. minetest.pos_to_string(p2))
 	for i in area:iterp(utility.sort_positions(p1, p2)) do
 		nodes[i] = c_air
-        --print("doing " .. i)
+		--print("doing " .. i)
 	end
-    --print("test11")
+	--print("test11")
 	nodes[area:indexp(p0)] = minetest.get_content_id(topnode)
-    --print("test7")
+	--print("test7")
 	manip:set_data(nodes)
 	manip:write_to_map()
 	manip:update_map() -- <— this takes time
 	--print("test8")
-    local timer = minetest.get_node_timer( p0 )
-    timer:start( 1 )
-    --print("test9")
+	local timer = minetest.get_node_timer( p0 )
+	timer:start( 1 )
+	--print("test9")
 end

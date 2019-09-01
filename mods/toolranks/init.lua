d30f0f4777fa0162b38872696ef8de78682d0cb7
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-09-01 12:36:13 -0500

    Toolranks: don't spam text+sound

--- mods/toolranks/init.lua
+++ mods/toolranks/init.lua
@@ -171,10 +171,14 @@ function toolranks.new_afteruse(itemstack, user, node, digparams)
 		end
 	end
 
-  if(itemstack:get_wear() > 60135) then
-    minetest.chat_send_player(pname, "# Server: Your tool is about to break!")
-    ambiance.sound_play("default_tool_breaks", user:get_pos(), 1.0, 20)
-  end
+	-- Don't print message more than once every 5 seconds.
+	if os.time() > (pdata.last_alert or 0) + 5 then
+		if(itemstack:get_wear() > 60135) then
+			minetest.chat_send_player(pname, "# Server: Your tool is about to break!")
+			ambiance.sound_play("default_tool_breaks", user:get_pos(), 1.0, 20)
+			pdata.last_alert = os.time()
+		end
+	end
 
 	-- pass total number of nodes (of this type) that could be dug assuming no tool repairs, as second param to this function
   local level = toolranks.get_level(dugnodes, math.floor(65535 / digparams.wear), lastlevel)

69370d6c6dcbd080e5c4cacd70d65438ae7d26ef
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2019-09-04 23:11:21 -0500

    Make framework more generic

--- mods/dirtspread/registrations.lua
+++ mods/dirtspread/registrations.lua
@@ -3,134 +3,242 @@
 
 local INTERACTION_DATA = {
 	["default:dirt"] = {
+		-- Node turns to this if buried.
 		if_buried = "darkage:darkdirt",
 
-		when_group_lava_nearby = "darkage:darkdirt",
-		when_group_fire_nearby = "default:dry_dirt",
+		-- The key name doesn't actually matter, it can be anything,
+		-- as long as it begins with "when_" and ends with "_near".
+		when_group_lava_near = {
+			nodenames = "group:lava",
+			if_nearby = "darkage:darkdirt",
+		},
 
-		when_group_ice_above = "default:permafrost",
-		when_group_ice_below = "default:permafrost",
-		when_group_ice_nearby = "default:permafrost",
+		when_group_fire_near = {
+			nodenames = "group:fire",
+			if_nearby = "default:dry_dirt",
+		},
 
-		when_group_snow_above = "default:dirt_with_snow",
-		when_group_snow_below = "default:permafrost",
-		when_group_snow_nearby = "default:dirt_with_snow",
+		when_group_ice_near = {
+			nodenames = "group:ice",
+			if_nearby = "default:permafrost",
+		},
 
-		when_group_snowy_nearby = "default:dirt_with_snow",
+		when_group_snow_near = {
+			nodenames = {"group:snow", "group:snowy"},
+			if_above = "default:dirt_with_snow",
+			if_below = "default:permafrost",
+			if_nearby = "default:dirt_with_snow",
+		},
 
-		when_group_sand_above = "darkage:darkdirt",
-		when_group_sand_below = "default:dry_dirt",
-		when_group_sand_nearby = "default:dry_dirt",
+		when_group_sand_near = {
+			nodenames = "group:sand",
+			if_above = "darkage:darkdirt",
+			if_below = "default:dry_dirt",
+			if_nearby = "default:dry_dirt",
+		},
 
-		when_default_dirt_with_grass_nearby = function(pos, light, name, def, groups)
-			if light < 13 then
-				return "", true -- Wait a bit.
-			end
+		when_default_dirt_with_grass_near = {
+			nodenames = {"default:dirt_with_grass", "default:dirt_with_grass_footsteps"},
 
-			return "default:dirt_with_grass", false -- Done.
-		end,
+			if_nearby = function(pos, light, name, def, groups)
+				if light < 13 then
+					return "", true -- Wait a bit.
+				end
 
-		when_default_dirt_with_dry_grass_nearby = function(pos, light, name, def, groups)
-			if light < 13 then
-				return "", true -- Wait a bit.
-			end
+				return "default:dirt_with_grass", false -- Done.
+			end,
+		},
 
-			return "default:dirt_with_dry_grass", false -- Done.
-		end,
+		when_default_dirt_with_dry_grass_near = {
+			nodenames = "default:dirt_with_dry_grass",
 
-		when_moregrass_darkgrass_nearby = function(pos, light, name, def, groups)
-			if light < 13 then
-				return "", true -- Wait a bit.
-			end
+			if_nearby = function(pos, light, name, def, groups)
+				if light < 13 then
+					return "", true -- Wait a bit.
+				end
 
-			return "moregrass:darkgrass", false -- Done.
-		end,
+				return "default:dirt_with_dry_grass", false -- Done.
+			end,
+		},
+
+		when_moregrass_darkgrass_near = {
+			nodenames = "moregrass:darkgrass",
+
+			if_nearby = function(pos, light, name, def, groups)
+				if light < 13 then
+					return "", true -- Wait a bit.
+				end
+
+				return "moregrass:darkgrass", false -- Done.
+			end,
+		},
 
 		-- Shall return the nodename to set, or "" to leave unchanged.
 		-- Return boolean second parameter to indicate whether to wait.
-		when_group_flora_above = function(pos, light, name, def, groups)
-			if groups.junglegrass and groups.junglegrass > 0 then
-				if light >= 13 then
-					return "moregrass:darkgrass", false
-				else
-					return "", true
-				end
-			elseif groups.dry_grass and groups.dry_grass > 0 then
-				if light >= 13 then
-					return "default:dirt_with_dry_grass", false
-				else
-					return "", true
-				end
-			elseif groups.grass and groups.grass > 0 then
-				if light >= 13 then
-					return "default:dirt_with_grass", false
-				else
-					return "", true
-				end
-			end
+		when_group_flora_near = {
+			nodenames = "group:flora",
 
-			return "", false -- Nothing to be done.
-		end,
+			if_above = function(pos, light, name, def, groups)
+				if groups.junglegrass and groups.junglegrass > 0 then
+					if light >= 13 then
+						return "moregrass:darkgrass", false
+					else
+						return "", true
+					end
+				elseif groups.dry_grass and groups.dry_grass > 0 then
+					if light >= 13 then
+						return "default:dirt_with_dry_grass", false
+					else
+						return "", true
+					end
+				elseif groups.grass and groups.grass > 0 then
+					if light >= 13 then
+						return "default:dirt_with_grass", false
+					else
+						return "", true
+					end
+				end
+
+				return "", false -- Nothing to be done.
+			end,
+		},
 	},
 
 	["default:dirt_with_grass"] = {
 		if_buried = "default:dirt",
 		if_covered = "default:dirt",
 
-		when_group_lava_nearby = "darkage:darkdirt",
-		when_group_fire_nearby = "default:dirt_with_dry_grass",
+		when_group_lava_near = {
+			nodenames = "group:lava",
+			if_nearby = "darkage:darkdirt",
+		},
 
-		when_group_snow_above = "default:dirt_with_snow",
-		when_group_ice_below = "default:permafrost_with_moss",
+		when_group_fire_near = {
+			nodenames = "group:fire",
+			if_nearby = "default:dirt_with_dry_grass",
+		},
+
+		when_group_snow_near = {
+			nodenames = "group:snow",
+			if_above = "default:dirt_with_snow",
+		},
+
+		when_group_ice_near = {
+			nodenames = "group:ice",
+			if_below = "default:permafrost_with_moss",
+		},
 	},
 
 	["default:dirt_with_dry_grass"] = {
 		if_buried = "default:dirt",
 		if_covered = "default:dirt",
 
-		when_group_lava_nearby = "darkage:darkdirt",
-		when_group_fire_nearby = "default:dirt",
+		when_group_lava_near = {
+			nodenames = "group:lava",
+			if_nearby = "darkage:darkdirt",
+		},
 
-		when_group_snow_above = "default:dirt_with_snow",
-		when_group_ice_below = "default:permafrost_with_moss",
+		when_group_fire_near = {
+			nodenames = "group:fire",
+			if_nearby = "default:dirt",
+		},
+
+		when_group_snow_near = {
+			nodenames = "group:snow",
+			if_above = "default:dirt_with_snow",
+		},
+
+		when_group_ice_near = {
+			nodenames = "group:ice",
+			if_below = "default:permafrost_with_moss",
+		},
 	},
 
 	["moregrass:darkgrass"] = {
 		if_buried = "default:dirt",
 		if_covered = "default:dirt",
 
-		when_group_lava_nearby = "darkage:darkdirt",
-		when_group_fire_nearby = "default:dirt_with_dry_grass",
+		when_group_lava_near = {
+			nodenames = "group:lava",
+			if_nearby = "darkage:darkdirt",
+		},
 
-		when_group_snow_above = "default:dirt_with_snow",
-		when_group_ice_below = "default:permafrost_with_moss",
+		when_group_fire_near = {
+			nodenames = "group:fire",
+			if_nearby = "default:dirt_with_dry_grass",
+		},
+
+		when_group_snow_near = {
+			nodenames = "group:snow",
+			if_above = "default:dirt_with_snow",
+		},
+
+		when_group_ice_near = {
+			nodenames = "group:ice",
+			if_below = "default:permafrost_with_moss",
+		},
 	},
 
 	["default:dirt_with_snow"] = {
 		if_buried = "default:dirt",
 		if_covered = "default:dirt",
 
-		when_group_lava_nearby = "darkage:darkdirt",
-		when_group_fire_nearby = "default:dirt",
+		when_group_lava_near = {
+			nodenames = "group:lava",
+			if_nearby = "darkage:darkdirt",
+		},
+
+		when_group_fire_near = {
+			nodenames = "group:fire",
+			if_nearby = "default:dirt",
+		},
 	},
 
 	["default:permafrost"] = {
-		when_group_lava_nearby = "darkage:darkdirt",
-		when_group_fire_nearby = "default:dirt",
+		when_group_lava_near = {
+			nodenames = "group:lava",
+			if_nearby = "darkage:darkdirt",
+		},
 
-		when_group_snow_above = "default:permafrost_with_snow",
-		when_group_flora_above = "default:permafrost_with_moss",
+		when_group_fire_near = {
+			nodenames = "group:fire",
+			if_nearby = "default:dirt",
+		},
+
+		when_group_snow_near = {
+			nodenames = "group:snow",
+			if_above = "default:permafrost_with_snow",
+		},
+
+		when_group_flora_near = {
+			nodenames = "group:flora",
+			if_above = "default:permafrost_with_moss",
+		},
 	},
 
 	["default:permafrost_with_moss"] = {
 		if_buried = "default:permafrost",
 		if_covered = "default:permafrost",
 
-		when_group_lava_nearby = "darkage:darkdirt",
-		when_group_fire_nearby = "default:permafrost",
+		when_group_lava_near = {
+			nodenames = "group:lava",
+			if_nearby = "darkage:darkdirt",
+		},
 
-		when_group_snow_above = "default:permafrost_with_snow",
-		when_default_cobble_above = "default:permafrost_with_stones",
+		when_group_fire_near = {
+			nodenames = "group:fire",
+			if_nearby = "default:permafrost",
+		},
+
+		when_group_snow_near = {
+			nodenames = "group:snow",
+			if_above = "default:permafrost_with_snow",
+		},
+
+		when_default_cobble_near = {
+			nodenames = "default:cobble",
+			if_above = "default:permafrost_with_stones",
+		},
 	},
 }
 
@@ -206,7 +314,7 @@ local HANDLER = function(pos, node)
 	end
 
 	-- Get what's to the 4 sides (not including center or corners).
-	local neighbors = {
+	local all_neighbors = {
 		-- Adjacent horizontal sides.
 		{x=pos.x-1, y=pos.y,   z=pos.z  },
 		{x=pos.x+1, y=pos.y,   z=pos.z  },
@@ -251,9 +359,17 @@ local HANDLER = function(pos, node)
 	}
 
 	-- This is needed so that the order in which neighbors are checked is random.
-	table.shuffle(neighbors)
+	table.shuffle(all_neighbors)
 
-	local find_nearby = function(names)
+	local neighbors_above = {
+		{x=pos.x, y=pos.y+1, z=pos.z},
+	}
+
+	local neighbors_below = {
+		{x=pos.x, y=pos.y-1, z=pos.z},
+	},
+
+	local find_nearby = function(neighbors, names)
 		if type(names) == "string" then
 			for k, v in ipairs(neighbors) do
 				local n2 = minetest.get_node(v)
@@ -274,16 +390,15 @@ local HANDLER = function(pos, node)
 		elseif type(names) == "table" then
 			for k, v in ipairs(neighbors) do
 				local n2 = minetest.get_node(v)
+				local d2
+				local g2
 				for _, n in ipairs(names) do
 					if n2.name == n then
 						return v
 					elseif string.find(n, "^group:") then
 						local g = string.sub(n, string.len("group:") + 1)
-						local d2 = minetest.registered_nodes[n2.name]
-						local g2 = {}
-						if d2 then
-							g2 = d2.groups or {}
-						end
+						d2 = d2 or minetest.registered_nodes[n2.name]
+						g2 = g2 or d2.groups or {}
 						if g2[g] and g2[g] > 0 then
 							return v
 						end
@@ -293,147 +408,64 @@ local HANDLER = function(pos, node)
 		end
 	end
 
-	local execute_action = function(is_group, gkey, node_or_group)
-		local above_key = "when_" .. gkey .. "_above"
-		local below_key = "when_" .. gkey .. "_below"
-		local nearby_key = "when_" .. gkey .. "_nearby"
-
-		-- Action above.
-		if interaction_data[above_key] then
-			local n2 = minetest.get_node(above)
-			local d2 = minetest.registered_nodes[n2.name]
-			local g2 = {}
-			if d2 then
-				g2 = d2.groups or {}
-			end
-			local good = false
-			if is_group then
-				if g2[node_or_group] and g2[node_or_group] > 0 then
-					good = true
-				end
-			else
-				if n2.name == node_or_group then
-					good = true
-				end
-			end
-			if good then
-				if type(interaction_data[above_key]) == "string" then
-					node.name = interaction_data[above_key]
-					minetest.add_node(pos, node)
-					return false, true -- Don't wait, done.
-				elseif type(interaction_data[above_key]) == "function" then
-					local ret, wait = interaction_data[above_key](pos, light_above, n2.name, d2, g2)
+	local function do_nodecheck(callback, neighbors, nodenames)
+		local p2 = find_nearby(neighbors, nodenames)
+		if p2 then
+			if type(callback) == "string" then
+				node.name = callback
+				minetest.add_node(pos, node)
+				return false, true -- Don't wait, done.
+			elseif type(callback == "function" then
+				local n2 = minetest.get_node(p2)
+				local d2 = minetest.registered_nodes[n2.name]
+				if d2 then
+					local g2 = d2.groups or {}
+					local ret, wait = callback(pos, light_above, n2.name, d2, g2)
 					if ret and ret ~= "" then
 						node.name = ret
 						minetest.add_node(pos, node)
 						return false, true -- Don't wait, done.
 					elseif wait then
 						return true, false -- Wait, not done.
-					end
-				end
-			end
-		end
-
-		-- Action below.
-		if interaction_data[below_key] then
-			local n2 = minetest.get_node(below)
-			local d2 = minetest.registered_nodes[n2.name]
-			local g2 = {}
-			if d2 then
-				g2 = d2.groups or {}
-			end
-			local good = false
-			if is_group then
-				if g2[node_or_group] and g2[node_or_group] > 0 then
-					good = true
-				end
-			else
-				if n2.name == node_or_group then
-					good = true
-				end
-			end
-			if good then
-				if type(interaction_data[below_key]) == "string" then
-					node.name = interaction_data[below_key]
-					minetest.add_node(pos, node)
-					return false, true -- Don't wait, done.
-				elseif type(interaction_data[below_key]) == "function" then
-					local ret, wait = interaction_data[below_key](pos, light_above, n2.name, d2, g2)
-					if ret and ret ~= "" then
-						node.name = ret
-						minetest.add_node(pos, node)
-						return false, true -- Don't wait, done.
-					elseif wait then
-						return true, false -- Wait, not done.
-					end
-				end
-			end
-		end
-
-		-- Action nearby.
-		if interaction_data[nearby_key] then
-			local find_name = node_or_group
-			if is_group then
-				find_name = "group:" .. find_name
-			end
-			local p2 = find_nearby(find_name)
-			if p2 then
-				if type(interaction_data[nearby_key]) == "string" then
-					node.name = interaction_data[nearby_key]
-					minetest.add_node(pos, node)
-					return false, true -- Don't wait, done.
-				elseif type(interaction_data[nearby_key]) == "function" then
-					local n2 = minetest.get_node(p2)
-					local d2 = minetest.registered_nodes[n2.name]
-					if d2 then
-						local g2 = d2.groups or {}
-						local ret, wait = interaction_data[nearby_key](pos, light_above, n2.name, d2, g2)
-						if ret and ret ~= "" then
-							node.name = ret
-							minetest.add_node(pos, node)
-							return false, true -- Don't wait, done.
-						elseif wait then
-							return true, false -- Wait, not done.
-						end
 					end
 				end
 			end
 		end
 	end
 
-	-- Keep track of which keys have already been processed.
-	local done_parts = {}
+	local execute_action = function(data)
+		-- Action above.
+		if data.if_above then
+			local wait, done = do_nodecheck(data.if_above, neighbors_above, data.nodenames)
+			if wait or done then
+				return wait, done
+			end
+		end
+
+		-- Action below.
+		if data.if_below then
+			local wait, done = do_nodecheck(data.if_below, neighbors_below, data.nodenames)
+			if wait or done then
+				return wait, done
+			end
+		end
+
+		-- Action nearby.
+		if data.if_nearby then
+			local wait, done = do_nodecheck(data.if_nearby, all_neighbors, data.nodenames)
+			if wait or done then
+				return wait, done
+			end
+		end
+	end
 
 	for key, data in pairs(interaction_data) do
-		if key:find("^when_") then
-			if key:find("_above$") or key:find("_below$") or key:find("_nearby$") then
-				local _, first = key:find("^when_group_")
-				local is_group = true
-				if not first then
-					_, first = key:find("^when_")
-					is_group = false
-				end
-				first = first + 1
-				local last = key:find("_[^_]*$") - 1
-				local part = key:sub(first, last)
-				local gkey = part
-				if not is_group then
-					-- Need to replace the first '_' in `part` with a ':'.
-					local p = part:find('_')
-					part = part:sub(1, p-1) .. ':' .. part:sub(p+1)
-				else
-					gkey = "group_" .. gkey
-				end
-				if not done_parts[part] then
-					minetest.chat_send_all(gkey .. ", " .. part)
-					local wait, done = execute_action(is_group, gkey, part)
-					if wait then
-						return true
-					elseif done then
-						return
-					end
-					done_parts[part] = true
-				end
+		if key:find("^when_") and key:find("_near$") then
+			local wait, done = execute_action(data)
+			if wait then
+				return true
+			elseif done then
+				return
 			end
 		end
 	end

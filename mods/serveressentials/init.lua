b218b681435cb6aa353f01a6576eab57d98feda7
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2020-02-13 10:15:41 -0600

    Add automatic realm reset, starting at 30 days

--- mods/serveressentials/init.lua
+++ mods/serveressentials/init.lua
@@ -1,8 +1,38 @@
 
 serveressentials = serveressentials or {}
 serveressentials.modpath = minetest.get_modpath("serveressentials")
+serveressentials.modstorage = minetest.get_mod_storage()
 
 dofile(serveressentials.modpath .. "/outback.lua")
+
+function serveressentials.check_outback_reset()
+	local meta = serveressentials.modstorage
+	local stime = meta:get_string("outback_reset_time")
+
+	-- If timestamp is missing, then initialize it to the current time.
+	-- Outback reset will be schedualed after the timeout.
+	if not stime or stime == "" then
+		stime = tostring(os.time())
+		meta:set_string("outback_reset_time", stime)
+		return
+	end
+
+	local time = tonumber(stime) -- Time of last reset (or initialization).
+	local days = 30 -- Timeout in days.
+	local timeout = 60 * 60 * 24 * days
+	local now = os.time() -- Current time.
+	local later = time + timeout -- Time of next reset.
+
+	if now >= later then
+		stime = tostring(later)
+		meta:set_string("outback_reset_time", stime)
+
+		serveressentials.rebuild_outback()
+		minetest.chat_send_all("# Server: The desert wind ceases to blow across the Outback.")
+		minetest.chat_send_all("# Server: Then it resumes, and there is nothing left of what went before.")
+	end
+end
+minetest.after(0, function() serveressentials.check_outback_reset() end)
 
 function serveressentials.acacia_fixup(pos)
 	local n1 = minetest.get_node(pos)

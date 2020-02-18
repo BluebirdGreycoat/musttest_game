008331be4bc0cb4111f0bb8c07a16015e93d8df8
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2020-02-18 01:58:46 -0600

    Add secret stone and rackstone trapdoors, unify trapdoor recipes

--- mods/doors/trapdoors.lua
+++ mods/doors/trapdoors.lua
@@ -14,12 +14,12 @@ doors.register_trapdoor("doors:trapdoor_locked", {
 	wield_image = "doors_trapdoor.png",
 	tile_front = "doors_trapdoor.png",
 	tile_side = "doors_trapdoor_side.png",
-    protected = true,
+	protected = true,
 	groups = utility.dig_groups("door_wood", {flammable = 2, door = 1}),
 })
 
 doors.register_trapdoor("doors:trapdoor_steel", {
-	description = "Iron Trapdoor",
+	description = "Locked Iron Trapdoor",
 	inventory_image = "doors_trapdoor_steel.png",
 	wield_image = "doors_trapdoor_steel.png",
 	tile_front = "doors_trapdoor_steel.png",
@@ -28,6 +28,38 @@ doors.register_trapdoor("doors:trapdoor_steel", {
 	sounds = default.node_sound_metal_defaults(),
 	sound_open = "doors_steel_door_open",
 	sound_close = "doors_steel_door_close",
+	groups = utility.dig_groups("door_metal", {door = 1}),
+})
+
+doors.register_trapdoor("doors:trapdoor_steel_unlocked", {
+	description = "Iron Trapdoor",
+	inventory_image = "doors_trapdoor_steel.png",
+	wield_image = "doors_trapdoor_steel.png",
+	tile_front = "doors_trapdoor_steel.png",
+	tile_side = "doors_trapdoor_steel_side.png",
+	sounds = default.node_sound_metal_defaults(),
+	sound_open = "doors_steel_door_open",
+	sound_close = "doors_steel_door_close",
+	groups = utility.dig_groups("door_metal", {door = 1}),
+})
+
+doors.register_trapdoor("doors:trapdoor_stone", {
+	description = "Stone Trapdoor",
+	inventory_image = "doors_trapdoor_stone.png",
+	wield_image = "doors_trapdoor_stone.png",
+	tile_front = "doors_trapdoor_stone.png",
+	tile_side = "doors_trapdoor_stone_side.png",
+	sounds = default.node_sound_stone_defaults(),
+	groups = utility.dig_groups("door_metal", {door = 1}),
+})
+
+doors.register_trapdoor("doors:trapdoor_rackstone", {
+	description = "Rackstone Trapdoor",
+	inventory_image = "doors_trapdoor_rackstone.png",
+	wield_image = "doors_trapdoor_rackstone.png",
+	tile_front = "doors_trapdoor_rackstone.png",
+	tile_side = "doors_trapdoor_rackstone_side.png",
+	sounds = default.node_sound_stone_defaults(),
 	groups = utility.dig_groups("door_metal", {door = 1}),
 })
 
@@ -59,42 +91,66 @@ doors.register_trapdoor("doors:trapdoor_iron", {
 minetest.register_craft({
 	output = 'doors:trapdoor 2',
 	recipe = {
-		{'default:wood', 'default:wood', 'default:wood'},
-		{'default:wood', 'default:wood', 'default:wood'},
-		{'', '', ''},
+		{'default:wood', 'default:wood', 'group:stick'},
+		{'default:wood', 'default:wood', 'group:stick'},
 	}
 })
 
 minetest.register_craft({
 	output = 'doors:trapdoor_locked',
 	recipe = {
-		{'default:wood', 'default:padlock', 'default:wood'},
-		{'default:wood', 'default:wood',        'default:wood'},
-		{'', '', ''},
+		{'', 'default:padlock', ''},
+		{'default:wood', 'default:wood', 'group:stick'},
+		{'default:wood', 'default:wood', 'group:stick'},
 	}
 })
 
 minetest.register_craft({
 	output = 'doors:trapdoor_steel',
 	recipe = {
-		{'', 'default:padlock'},
-		{'default:steel_ingot', 'default:steel_ingot'},
-		{'default:steel_ingot', 'default:steel_ingot'},
+		{'', 'default:padlock', ''},
+		{'default:steel_ingot', 'default:steel_ingot', 'group:stick'},
+		{'default:steel_ingot', 'default:steel_ingot', 'group:stick'},
+	}
+})
+
+minetest.register_craft({
+	output = 'doors:trapdoor_steel_unlocked',
+	recipe = {
+		{'default:steel_ingot', 'default:steel_ingot', 'group:stick'},
+		{'default:steel_ingot', 'default:steel_ingot', 'group:stick'},
+	}
+})
+
+minetest.register_craft({
+	output = 'doors:trapdoor_rackstone',
+	recipe = {
+		{'rackstone:rackstone', 'rackstone:rackstone', 'group:stick'},
+		{'rackstone:rackstone', 'rackstone:rackstone', 'group:stick'},
+	}
+})
+
+minetest.register_craft({
+	output = 'doors:trapdoor_stone',
+	recipe = {
+		{'default:stone', 'default:stone', 'group:stick'},
+		{'default:stone', 'default:stone', 'group:stick'},
 	}
 })
 
 minetest.register_craft({
 	output = 'doors:trapdoor_iron',
 	recipe = {
-		{'default:iron_lump', 'default:iron_lump', 'default:iron_lump'},
-		{'default:iron_lump', 'default:iron_lump', 'default:iron_lump'},
+		{'default:iron_lump', 'default:iron_lump', 'group:stick'},
+		{'default:iron_lump', 'default:iron_lump', 'group:stick'},
 	}
 })
 
 minetest.register_craft({
 	output = 'doors:trapdoor_iron_locked',
 	recipe = {
-		{'default:iron_lump', 'default:padlock', 'default:iron_lump'},
-		{'default:iron_lump', 'default:iron_lump',   'default:iron_lump'},
+		{'', 'default:padlock', ''},
+		{'default:iron_lump', 'default:iron_lump', 'group:stick'},
+		{'default:iron_lump', 'default:iron_lump', 'group:stick'},
 	}
 })

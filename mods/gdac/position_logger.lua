e4ec8f936154f86c6223e08f71d0fea4cd093a50
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2020-08-13 19:36:10 -0500

    Fixes

--- mods/gdac/position_logger.lua
+++ mods/gdac/position_logger.lua
@@ -2,13 +2,22 @@
 gdac.position_logger_path = minetest.get_worldpath() .. "/positions.txt"
 gdac.position_logger_players = gdac.position_logger_players or {}
 
-function gdac.position_logger_record(pname, pos, time)
+function gdac.position_logger_record(pname, pos, time, attached, sneak, sprint, jump)
 	-- Record format: player|20,190,-4|489128934
 	local s = pname .. "|" ..
 		math.floor(pos.x) .. "," .. math.floor(pos.y) .. "," .. math.floor(pos.z) .. "|" ..
-		time .. "\n"
+		time .. "|" .. attached .. "|" .. sneak .. "|" .. sprint .. "|" .. jump .. "\n"
 	gdac.position_logger_file:write(s)
 end
+
+local yes_attached = "attached"
+local not_attached = "no"
+local yes_sneak = "sneak"
+local not_sneak = "no"
+local yes_sprint = "sprint"
+local not_sprint = "no"
+local yes_jump = "jumping"
+local not_jump = "no"
 
 local time = 0
 function gdac.position_logger_step(dtime)
@@ -26,8 +35,25 @@ function gdac.position_logger_step(dtime)
 		local pos = pref:get_pos()
 
 		if not prev_pos or vector.distance(prev_pos, pos) >= 1 then
+			local ctrl = pref:get_player_controls()
+
+			local sneak = not_sneak
+			local attach = not_attached
+			local sprint = not_sprint
+			local jump = not_jump
+
+			if ctrl.sneak then
+				sneak = yes_sneak
+			end
+			if ctrl.aux1 then
+				sprint = yes_sprint
+			end
+			if ctrl.jump then
+				jump = yes_jump
+			end
+
 			local time = os.time()
-			gdac.position_logger_record(pname, pos, time)
+			gdac.position_logger_record(pname, pos, time, attach, sneak, sprint, jump)
 		end
 
 		gdac.position_logger_players[pname] = pos

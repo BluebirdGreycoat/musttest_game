2e3843129c98ca924c82cc24da0d523d2d54fe90
alauer <austinmlauer@hotmail.com>
2020-09-30 13:34:26 -0500

    trying again with coffee

--- mods/flowerpot_ex/init.lua
+++ mods/flowerpot_ex/init.lua
@@ -14,15 +14,15 @@ local plants = {
 	{name="blueberries:plant_2", drop="blueberries:fruit"},
 	{name="blueberries:plant_3", drop="blueberries:fruit"},
 	{name="blueberries:plant_4", drop="blueberries:fruit"},
-
+ 
 	{insert="blueberries:fruit", nodes={"blueberries:plant_1", "blueberries:plant_2", "blueberries:plant_3", "blueberries:plant_4"}},
 	
-	{name="coffee_bush:plant_1", drop="coffee_bush:seeds"},
-	{name="coffee_bush:plant_2", drop="coffee_bush:seeds"},
-	{name="coffee_bush:plant_3", drop="coffee_bush:seeds"},
-	{name="coffee_bush:plant_4", drop="coffee_bush:seeds"},
+	{name="coffee_bush:plant_1", drop="coffeebeans:seeds"},
+	{name="coffee_bush:plant_2", drop="coffeebeans:seeds"},
+	{name="coffee_bush:plant_3", drop="coffeebeans:seeds"},
+	{name="coffee_bush:plant_4", drop="coffeebeans:seeds"},
 
-	{insert="coffee_bush:seeds", nodes={"coffee_bush:plant_1", "coffee_bush:plant_2", "coffee_bush:plant_3", "coffee_bush:plant_4"}},
+	{insert="coffeebeans:seeds", nodes={"coffee_bush:plant_1", "coffee_bush:plant_2", "coffee_bush:plant_3", "coffee_bush:plant_4"}},
 
 
 	{name="raspberries:plant_1", drop="raspberries:fruit"},

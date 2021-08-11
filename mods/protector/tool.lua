f64bf62a61ede91de54561bbccc9e6aeee30e5f7
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2021-08-11 08:57:47 -0500

    Add a protector mover tool

--- mods/protector/tool.lua
+++ mods/protector/tool.lua
@@ -220,3 +220,156 @@ minetest.register_craft({
 		{"nodeinspector:nodeinspector"},
 	}
 })
+
+
+
+minetest.register_craftitem("protector:tool2", {
+	description = "Protector Mover Tool\n\nStand near protector, face direction and use.",
+	inventory_image = "nodeinspector.png^protector_lock.png",
+	stack_max = 1,
+
+	on_use = function(itemstack, user, pointed_thing)
+
+		local name = user:get_player_name()
+
+		-- check for protector near player (2 block radius)
+		local pos = vector_round(user:get_pos())
+		local pp = minetest.find_nodes_in_area(
+			vector.subtract(pos, 2), vector.add(pos, 2),
+			{"protector:protect", "protector:protect2",
+			"protector:protect3", "protector:protect4"})
+
+		if #pp == 0 then return end -- none found
+
+		if #pp > 1 then
+			minetest.chat_send_player(name, "# Server: Too many protectors nearby, choice would be ambiguous.")
+			return
+		end
+
+		pos = pp[1] -- take position of first protector found
+
+		-- get members on protector
+		local meta = minetest.get_meta(pos)
+		local members = meta:get_string("members") or ""
+		local owner = meta:get_string("owner") or ""
+		local placedate = meta:get_string("placedate") or ""
+
+		-- require the tool user to be the owner of the initial protector node
+		if not minetest.check_player_privs(name, {protection_bypass=true}) then
+			if owner ~= name then
+				minetest.chat_send_player(name, "# Server: Cannot expand claim from origin, the protector is not yours!")
+				return
+			end
+		end
+
+		-- get direction player is facing
+		local dir = minetest.dir_to_facedir( user:get_look_dir() )
+		local vec = {x = 0, y = 0, z = 0}
+		local gap = 1
+		local pit = user:get_look_vertical()
+
+		-- set placement coords
+		if pit > 1.2 then
+			vec.y = -gap -- up
+		elseif pit < -1.2 then
+			vec.y = gap -- down
+		elseif dir == 0 then
+			vec.z = gap -- north
+		elseif dir == 1 then
+			vec.x = gap -- east
+		elseif dir == 2 then
+			vec.z = -gap -- south
+		elseif dir == 3 then
+			vec.x = -gap -- west
+		end
+
+		-- new position (save old position)
+		local oldpos = {x=pos.x, y=pos.y, z=pos.z}
+		pos.x = pos.x + vec.x
+		pos.y = pos.y + vec.y
+		pos.z = pos.z + vec.z
+
+		-- ensure position is within a valid realm
+		if not rc.is_valid_realm_pos(pos) then
+			minetest.chat_send_player(name, "# Server: Cannot place protector in the Void!")
+			return
+		end
+		if not minetest.get_node_or_nil(pos) then
+			minetest.chat_send_player(name, "# Server: Cannot place protector within IGNORE!")
+			return
+		end
+
+		-- does placing a protector overlap existing area
+		-- this is the most important check! must not mess this up!
+		local success, reason = protector.check_overlap_main(protname, owner, pos)
+		if not success then
+			if reason == 1 then
+				minetest.chat_send_player(name, "# Server: Protection bounds overlap into another person's area claim.")
+			elseif reason == 2 then
+				minetest.chat_send_player(name, "# Server: You cannot claim this area while someone's fresh corpse is nearby!")
+			elseif reason == 3 then
+				minetest.chat_send_player(name, "# Server: You must remove all corpses before you can claim this area.")
+			else
+				minetest.chat_send_player(name, "# Server: Cannot place protection for unknown reason.")
+			end
+			return
+		end
+
+		-- does a protector already exist ?
+		if #minetest.find_nodes_in_area(vector.subtract(pos, 0), vector.add(pos, 0),
+				{"protector:protect", "protector:protect2", "protector:protect3", "protector:protect4"}) > 0 then
+			minetest.chat_send_player(name, "# Server: Protector already in place!")
+			return
+		end
+
+		-- do not replace containers with inventory space
+		if minetest.get_inventory({type = "node", pos = pos}) then
+			minetest.chat_send_player(name, "# Server: Cannot place protector, container at " .. rc.pos_to_namestr(pos) .. ".")
+			return
+		end
+
+		-- protection check for other stuff, like bedrock, etc
+		if minetest.is_protected(pos, owner) then
+			minetest.chat_send_player(name, "Cannot place protector, already protected at " .. rc.pos_to_namestr(pos) .. ".")
+			return
+		end
+
+		-- check not replacing an immovable object
+		local node = minetest.get_node(pos)
+		if minetest.get_item_group(node.name, "immovable") ~= 0 then
+			minetest.chat_send_player(name, "# Server: Cannot place protector in place of immovable object!")
+			return
+		end
+
+		local nod = minetest.get_node(oldpos).name
+
+		-- place protector
+		minetest.set_node(pos, {name = nod, param2 = 1})
+		minetest.remove_node(oldpos)
+
+		-- set protector metadata
+		local meta = minetest.get_meta(pos)
+		local dname = rename.gpn(owner)
+
+		meta:set_string("placedate", placedate)
+		meta:set_string("owner", owner)
+		meta:set_string("rename", dname)
+		meta:set_string("infotext", "Protection (Owned by <" .. dname .. ">!)\nPlaced on " .. placedate)
+		meta:set_string("members", members)
+
+		-- Notify nearby players.
+		protector.update_nearby_players(pos)
+
+		ambiance.sound_play(electric_screwdriver.sound, pos, electric_screwdriver.sound_gain, electric_screwdriver.sound_dist)
+		minetest.chat_send_player(name, "# Server: Protector moved to " .. rc.pos_to_namestr(pos) .. ".")
+	end,
+})
+
+-- tool recipe
+minetest.register_craft({
+	output = "protector:tool2",
+	recipe = {
+		{"nodeinspector:nodeinspector"},
+		{"protector:protect4"},
+	}
+})

d6bee54b7776befb13a868b67e905a9e548a391e
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2022-07-09 22:05:26 -0500

    Fix ingot-duplication exploit reported by player

--- mods/ingots/api.lua
+++ mods/ingots/api.lua
@@ -17,22 +17,25 @@
 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ]]--
 
--- takes an item name and a texture name and a boolean whether the ingots are big
+-- Takes an item name and a texture name and a boolean whether the ingots are
+-- big.
 function ingots.register_ingots(ingot_item, texture, is_big)
 
-	--checks, whether the item name is a valid item (thanks 'puzzlecube')
+	-- Checks, whether the item name is a valid item (thanks 'puzzlecube').
 	if not minetest.registered_items[ingot_item] then
 		minetest.log("warning", ingot_item.." is not registered. Skipping ingot registration")
 		return
 	end
 
-	-- de hardcoded modname, which allows the api to be properly used from within other mods (thanks 'argyle')
+	-- De-hardcoded modname, which allows the api to be properly used from within
+	-- other mods (thanks 'argyle').
 	local mod_name = minetest.get_current_modname()
 	local mod_prefix = mod_name .. ":"
 
 	local stack_size = 64
 	local texture_prefix = "ingot_"
-	--gets item name witout mod part, to be used in the deffinition of the new nodes
+	-- Gets item name witout mod part, to be used in the deffinition of the new
+	-- nodes.
 	local ingot_name = string.sub(ingot_item, string.find(ingot_item, ":", 1, true) +1, -1)
 
 	-- Some ingots do not have unique sub-names.
@@ -48,20 +51,22 @@ function ingots.register_ingots(ingot_item, texture, is_big)
 		texture_prefix = "ingot_big_"
 	end
 
-	--this way there is no need for a separate on_punch function for a stack of 1 ingot
+	-- This is no longer used by the mod, but required for compatibility.
 	minetest.register_alias(mod_prefix .. ingot_name .."_0", "air")
 
-	--gives the ingot_item the ability to be placed and increas already placed stacks of ingots
+	-- Gives the ingot_item the ability to be placed and increas already placed
+	-- stacks of ingots.
 	minetest.override_item(ingot_item, {
-		on_place = function (itemstack, placer, pointed_thing)
-            local pos = minetest.get_pointed_thing_position(pointed_thing, true)
-            if minetest.is_protected(pos, placer:get_player_name()) and not minetest.check_player_privs(placer, "protection_bypass") then
-			    return
-		    end
+		on_place = function(itemstack, placer, pointed_thing)
+			local pos = minetest.get_pointed_thing_position(pointed_thing, true)
+			if minetest.is_protected(pos, placer:get_player_name()) and not minetest.check_player_privs(placer, "protection_bypass") then
+				return
+			end
+
 			if pointed_thing["type"] == "node" then
 				local name = minetest.get_node(pointed_thing.under).name
-				-- call on_rightclick function of pointed node if aplicable and not sneak
-				-- might or might not break if item is placed by mod devices
+				-- Call on_rightclick function of pointed node if aplicable and not
+				-- sneak. Might or might not break if item is placed by mod devices.
 				if minetest.registered_nodes[name].on_rightclick and
 					not placer:get_player_control().sneak
 				then
@@ -99,15 +104,54 @@ function ingots.register_ingots(ingot_item, texture, is_big)
 		light_source = 6
 	end
 
-	--registers 'stack_size' number of nodes, each has one more ingot in it than the last
+	local function on_punch(pos, node_stale, puncher, pointed_thing)
+		if not puncher then
+			return
+		end
+
+		local wield = puncher:get_wielded_item()
+		-- Checks that a stack can be taken apart by hand or relevant "ingot_item".
+		if wield:get_name() == ingot_item or wield:get_count() == 0 then
+			if minetest.test_protection(pos, puncher:get_player_name()) then
+				return
+			end
+
+			-- Get actual node at this location. (Function call might be stale!)
+			-- As a falling node, the falling code might (usually will) run first,
+			-- which causes this function to misbehave if we don't check ....
+			local node = minetest.get_node(pos)
+			if node.name ~= node_stale.name then
+				return
+			end
+			local ndef = minetest.registered_nodes[node.name]
+			local count = ndef._ingot_count
+
+			if count > 1 then
+				minetest.set_node(pos, {
+					name = mod_prefix .. ingot_name .."_" .. count - 1,
+					param2 = node.param2,
+				})
+			else
+				minetest.remove_node(pos)
+				minetest.check_for_falling(pos)
+			end
+
+			local stack = ItemStack(ingot_item)
+			puncher:get_inventory():add_item("main", stack)
+		end
+	end
+
+	-- Registers 'stack_size' number of nodes, each has one more ingot in it than
+	-- the last.
 	for i = 1, stack_size do
 		local box = {
-					type = "fixed",
-					fixed = {
-						--rectangular box which encompases all placed ingots
-						ingots.get_box(is_big, i),
-					},
-				}
+			type = "fixed",
+			fixed = {
+				-- Rectangular box which encompases all placed ingots
+				ingots.get_box(is_big, i),
+			},
+		}
+
 		minetest.register_node(mod_prefix .. ingot_name .. "_" .. i,{
 			description = "ingots",
 			drawtype = "mesh",
@@ -125,33 +169,27 @@ function ingots.register_ingots(ingot_item, texture, is_big)
 			-- Horizontal rotation only!
 			on_rotate = screwdriver.rotate_simple,
 
-			groups = {cracky = 3, level = 2, not_in_creative_inventory = 1, falling_node = 1},
-			drop = ingot_item .. " " .. i,
-			on_punch = function (pos, node, puncher, pointed_thing)
-				if puncher then
-					local wield = puncher:get_wielded_item()
-					--checks, so that a stack can be taken appart only by hand or relevant ingot_item
-					if wield:get_name() == ingot_item or
-						wield:get_count() == 0 then
-                        if minetest.is_protected(pos, puncher:get_player_name()) and not minetest.check_player_privs(puncher, "protection_bypass") then
-			                return
-		                end
-						minetest.set_node(pos, {name = mod_prefix .. ingot_name .."_" .. i - 1, param2 = node.param2})
+			groups = {
+				cracky = 3,
+				level = 2,
+				not_in_creative_inventory = 1,
+				falling_node = 1,
+			},
 
-						local stack = ItemStack(ingot_item)
-						puncher:get_inventory():add_item("main", stack)
-					end
-				end
-			end,
+			drop = ingot_item .. " " .. i,
+			on_punch = on_punch,
+
 			_ingot_name = ingot_name,
 			_ingot_count = i,
 		})
 	end
 end
 
---returns an apropriate nodebox for a given number of ingots
+-- Returns an apropriate nodebox for a given number of ingots.
 function ingots.get_box(is_big, i)
-	if is_big then return {-0.5, -0.5, -0.5, 0.5, (((i + 1 - ((i +1 )%2)) / 8) - 0.5), 0.5}
-	else return {-0.5, -0.5, -0.5, 0.5, (((i - 1 - ((i-1)%8)) / 8) - 3) / 8, 0.5}
+	if is_big then
+		return {-0.5, -0.5, -0.5, 0.5, (((i + 1 - ((i +1 )%2)) / 8) - 0.5), 0.5}
+	else
+		return {-0.5, -0.5, -0.5, 0.5, (((i - 1 - ((i-1)%8)) / 8) - 3) / 8, 0.5}
 	end
 end

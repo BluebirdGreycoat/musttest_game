71c66e81adfcf8fb4da2cbd769f29482c1ca29ca
BluebirdGreycoat <bluebird.greycoat@gmail.com>
2022-08-21 23:15:52 -0500

    Register reload names for the  command-tokens code

--- mods/command_tokens/init.lua
+++ mods/command_tokens/init.lua
@@ -3,9 +3,10 @@ command_tokens = command_tokens or {}
 command_tokens.modpath = minetest.get_modpath("command_tokens")
 
 local path = command_tokens.modpath
-dofile(path.."/crafts.lua")
-dofile(path.."/mark_token.lua")
-dofile(path.."/mute_token.lua")
-dofile(path.."/jail_token.lua")
+dofile(path .. "/crafts.lua")
+
+reload.register_file("command_tokens:mark", path .. "/mark_token.lua", true)
+reload.register_file("command_tokens:mute", path .. "/mute_token.lua", true)
+reload.register_file("command_tokens:jail", path .. "/jail_token.lua", true)
 
 

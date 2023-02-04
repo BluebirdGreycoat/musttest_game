7cbae3798df07e5be19a225761f135e4f3ccf930
BlueBird51 <bluebird.greycoat@gmail.com>
2023-02-04 17:01:10 -0600

    Add brass wall/castle nodes

--- mods/extra_walls/init.lua
+++ mods/extra_walls/init.lua
@@ -2,3 +2,5 @@
 walls.register("bleached_cobble", "Bleached Cobble", "whitestone_cobble.png",
 		"whitestone:cobble", default.node_sound_stone_defaults())
 
+walls.register("brass", "Brass", "technic_brass_block.png",
+		"brass:block", default.node_sound_metal_defaults())

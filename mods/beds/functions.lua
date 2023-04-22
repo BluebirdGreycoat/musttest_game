bc381911dec3af555e4163e532d409c90db4a3c6
BlueBird51 <bluebird.greycoat@gmail.com>
2023-04-22 18:51:10 -0500

    Flying

--- mods/beds/functions.lua
+++ mods/beds/functions.lua
@@ -138,7 +138,7 @@ local function lay_down(player, pos, bed_pos, state, skip)
 		player:set_eye_offset({x = 0, y = 0, z = 0}, {x = 0, y = 0, z = 0})
 		player:set_look_horizontal(math_random(1, 180) / 100)
 		default.player_attached[name] = false
-		player:set_physics_override(1, 1, 1)
+		player:set_physics_override({speed = 1, jump = 1})
 		hud_flags.wielditem = true
 		default.player_set_animation(player, "stand" , 30)
 
@@ -153,8 +153,12 @@ local function lay_down(player, pos, bed_pos, state, skip)
 		local yaw, param2 = get_look_yaw(bed_pos)
 		player:set_look_horizontal(yaw)
 		local dir = minetest.facedir_to_dir(param2)
-		local p = {x = bed_pos.x + dir.x / 2, y = bed_pos.y, z = bed_pos.z + dir.z / 2}
-		player:set_physics_override(0, 0, 0)
+		local p = {
+			x = bed_pos.x + dir.x / 2,
+			y = bed_pos.y + 0.5,
+			z = bed_pos.z + dir.z / 2,
+		}
+		player:set_physics_override({speed = 0, jump = 0})
 		player:set_pos(p)
 		default.player_attached[name] = true
 		hud_flags.wielditem = false

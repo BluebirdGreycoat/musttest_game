a7ef68de3c329de77b352948a0055a79bf3062f8
BlueBird51 <bluebird.greycoat@gmail.com>
2023-07-19 23:36:45 -0500

    Mess with time again

--- mods/hb4/countdown.lua
+++ mods/hb4/countdown.lua
@@ -13,14 +13,11 @@ function countdown.step(data)
 
 	data = data or {hour = -1, min = -1, sec = -1}
 
-	-- Current time, as a number.
+	-- Current timestamp, as a number.
 	local ct = os.time()
-	local cd = os.date("!*t")
 
-	-- Datetime of next shutdown.
-	assert(countdown.time)
-	local nd = countdown.time
-	local nt = os.time(nd)
+	-- Timestamp of next shutdown.
+	local nt = countdown.time
 
 	-- If our restart time is in the past, we're done.
 	if nt <= ct then
@@ -31,8 +28,8 @@ function countdown.step(data)
 		return
 	end
 
-	assert(nt >= ct)
-	local rt = os.difftime(nt, ct)
+	-- Calculate remaining time (subtract future time in seconds from current time, also in seconds).
+	local rt = nt - ct
 	local rd = os.date("!*t", rt)
 
 	local report = false
@@ -41,6 +38,9 @@ function countdown.step(data)
 	-- Debug.
 	--delay = 10
 	--report = true
+	--minetest.chat_send_all("Seconds remaining: " .. rt)
+
+	local msgtype = 0
 
 	-- Calculate how long until next public message.
 	do
@@ -49,24 +49,27 @@ function countdown.step(data)
 			report = true
 		end
 
+		if rd.hour > 0 then
+			-- Report the nearest rounded hour.
+			msgtype = 3
+		end
+
+		if rd.hour == 0 and rd.min <= 60 then
+			msgtype = 2
+		end
+
 		-- Report every minute once there are 10 or less minutes.
 		if rd.hour == 0 and rd.min <= 10 and data.min ~= rd.min then
 			report = true
+			delay = 5
+			msgtype = 2
 		end
 
 		-- Report every second of the last 20 seconds.
-		if rd.hour == 0 and rd.min == 0 and rd.sec <= 20 then
+		if rd.hour == 0 and rd.min == 0 and rd.sec <= 20 and data.sec ~= rd.sec then
 			report = true
-		end
-
-		-- Reduce delay to 5 seconds once there are 10 minutes remaining to restart.
-		if rd.hour == 0 and rd.min <= 10 then
-			delay = 5
-		end
-
-		-- And reduce to 1 second when there are 20 or less seconds remaining.
-		if rd.hour == 0 and rd.min == 0 and rd.sec <= 20 then
 			delay = 1
+			msgtype = 1
 		end
 
 		-- Record time of last timecheck.
@@ -77,6 +80,22 @@ function countdown.step(data)
 
 	if report then
 		local message = "# Server: Nightly restart in: " .. string.format("%02d:%02d:%02d", rd.hour, rd.min, rd.sec) .. "."
+
+		if msgtype == 1 then
+			local p = "s"
+			if rd.sec == 1 then p = "" end
+			message = "Server: Restart in " .. rd.sec .. " second" .. p .. "."
+		elseif msgtype == 2 then
+			local p = "s"
+			if rd.min == 1 then p = "" end
+			message = "Server: Restarting in " .. rd.min .. " minute" .. p .. "."
+		elseif msgtype == 3 then
+			local h = rd.hour + math.floor((rd.min / 60) + 0.5)
+			local p = "s"
+			if h == 1 then p = "" end
+			message = "Server: Nightly restart in " .. h .. " hour" .. p .. "."
+		end
+
 		chat_logging.log_server_message(message)
 		minetest.chat_send_all(color .. message)
 	end
@@ -86,16 +105,18 @@ function countdown.step(data)
 end
 
 if not countdown.registered then
-	-- Calculate time to next shutdown.
-	local cd = os.date("!*t")
-	-- Should be 01:00:00 of next day UTC (1 hour after midnight, CST).
+	-- Get current timestamp as a datetime table.
+	local cd = os.date("*t")
+
+	-- Should be 1 hour after midnight, CST. 6 AM UTC.
 	local nd = table.copy(cd)
 	nd.day = nd.day + 1
-	nd.hour = 18
+	nd.hour = 1
 	nd.min = 0
 	nd.sec = 0
-	nd = os.date("!*t", os.time(nd))
-	countdown.time = nd
+
+	-- Store shutdown timestamp as time in seconds in the future.
+	countdown.time = os.time(nd)
 
 	local delay = 60
 

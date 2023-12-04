92f3fe8e232789dab0b02d9b15ded062837b3e1a
BlueBird51 <bluebird.greycoat@gmail.com>
2023-12-04 17:57:55 -0600

    Add required minimal entity/node/tool stubs

--- mods/anvil/init.lua
+++ mods/anvil/init.lua
@@ -9,6 +9,47 @@ anvil.modpath = minetest.get_modpath("anvil")
 if not anvil.registered then
 	anvil.registered = true
 
+	-- Display entity.
+	minetest.register_entity("anvil:item", {
+		initial_properties = {
+			visual = "item",
+			wield_item = "default:coal_lump",
+			visual_size = {x=0.4, y=0.4, z=0.4},
+			collide_with_objects = false,
+			pointable = false,
+			collisionbox = {0},
+		},
+	})
+
+	-- The node.
+	minetest.register_node("anvil:anvil", {
+		description = "Blacksmithing Anvil",
+		tiles = {
+			{name="nope.png"},
+			{name="nope.png"},
+			{name="nope.png"},
+			{name="nope.png"},
+			{name="nope.png"},
+			{name="nope.png"},
+		},
+
+		groups = utility.dig_groups("bigitem", {falling_node=1}),
+		drop = 'anvil:anvil',
+		sounds = default.node_sound_metal_defaults(),
+	})
+
+	-- Hammering tool.
+	minetest.register_tool("anvil:hammer", {
+		description = "Blacksmithing Hammer",
+		inventory_image = "anvil_tool_steelhammer.png",
+		wield_image = "anvil_tool_steelhammer.png",
+		tool_capabilities = tooldata["hammer_hammer"],
+		sound = {
+			breaks = "basictools_tool_breaks",
+		},
+		groups = {},
+	})
+
 	-- Register mod reloadable.
 	local c = "anvil:core"
 	local f = anvil.modpath .. "/init.lua"

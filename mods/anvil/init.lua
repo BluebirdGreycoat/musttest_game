14f97ec21185ddfc2af8f10a7964884dc50b9e38
BlueBird51 <bluebird.greycoat@gmail.com>
2023-12-06 00:26:52 -0600

    Finalize header comment on anvil mod

--- mods/anvil/init.lua
+++ mods/anvil/init.lua
@@ -1,7 +1,17 @@
 
 -- This is a cleanroom reimplementation of the GPL "anvil" mod commonly found
--- lying around. This reimplementation is developed from memory and in-game
--- (loose) behavior testing. It is provided under the MIT license.
+-- lying around. This reimplementation is developed from memory of how the anvil
+-- worked in-game, and through in-game behavior testing of THIS implementation
+-- as it was being delveloped. This mod is provided under the MIT license.
+--
+-- Licensing notice: by "cleanroom implementation", I (MustTest) declare that
+-- all code in this init.lua file was written WITHOUT REFERENCE to the code
+-- in the original anvil mod. Thus, this file is my own, separate expression of
+-- the concept of an anvil in Minetest. Certain similarities are required to
+-- achieve drop-in compatibility, they are as follows: the node name
+-- "anvil:anvil". The hammer name "anvil:hammer". The entity name "anvil:item",
+-- the metadata inventory list name, "input", and finally the group name
+-- "not_repaired_by_anvil".
 
 if not minetest.global_exists("anvil") then anvil = {} end
 anvil.modpath = minetest.get_modpath("anvil")

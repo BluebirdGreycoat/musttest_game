b1b93138ff734a3d553e3e7ab2b1d71280b760a4
BlueBird51 <bluebird.greycoat@gmail.com>
2023-12-21 20:23:27 -0600

    The Liar's punishment is not that no one believes him. If he is clever and lies judiciously, he will escape suspicion, and none will be the wiser. But the true punishment of the Liar is one which he cannot avoid no matter how intelligent he is, because it is part of his own nature. It is this: he eventually cannot trust anyone else.

--- mods/starpearl/init.lua
+++ mods/starpearl/init.lua
@@ -4,7 +4,16 @@ starpearl.modpath = minetest.get_modpath("starpearl")
 
 
 
-minetest.register_craftitem("starpearl:pearl", {
-	description = "Star Pearl",
-	inventory_image = "starpearl_pearl.png",
-})
+if not starpearl.run_once then
+	minetest.register_craftitem("starpearl:pearl", {
+		description = "Star Pearl",
+		inventory_image = "starpearl_pearl.png",
+	})
+
+  local c = "starpearl:core"
+  local f = starpearl.modpath .. "/init.lua"
+  reload.register_file(c, f, false)
+
+  starpearl.run_once = true
+end
+

30bbcbefcde10bf691fb229a29da025e83f702a9
BlueBird51 <bluebird.greycoat@gmail.com>
2024-07-01 13:10:30 -0500

    Support versioning

--- mods/ossl/api.lua
+++ mods/ossl/api.lua
@@ -18,19 +18,83 @@ end
 function ossl.encrypt(iv, text)
 	iv = minetest.decode_base64(iv)
 	assert(#iv == 16)
-	local data, err = ossl.cipher:encrypt(THEKEY, iv):final(text)
-	if data then
-		data = minetest.encode_base64(data)
+	local encrypted, err = ossl.cipher:encrypt(THEKEY, iv):final(text)
+	if encrypted then
+		-- Stick the encrypted data into a JSON table. This lets us handle
+		-- future versions (even key changes!), because we can check the
+		-- (unencrypted) JSON data for needed metadata.
+		local json = {
+			-- The encrypted data. Note: must b64 encode this because otherwise the
+			-- JSON encoder vomits on the raw binary.
+			enc = minetest.encode_base64(encrypted),
+
+			-- Version ID. Nil shall be treated the same as 1. 0 is the same as 1.
+			ver = 1,
+		}
+		if json.enc then
+			local serialized = minetest.write_json(json)
+			if serialized then
+				-- Stick a tag on it. Also, the final output string is base64 encoded
+				-- for safety with whatever APIs the string gets sent to afterward.
+				serialized = serialized .. ":JSON45"
+				encrypted = minetest.encode_base64(serialized)
+			else
+				err = "JSON encode error"
+				encrypted = nil
+			end
+		else
+			err = "base64 encode error"
+			encrypted = nil
+		end
 	end
-	return data, err
+	return encrypted, err
 end
 
 -- Input IV + encrypted binary, get plaintext; or nil + errormsg.
 -- The IV does not need to be secret and should live next to the binary.
 function ossl.decrypt(iv, data)
+	local decoded, err
+
 	iv = minetest.decode_base64(iv)
-	data = minetest.decode_base64(data)
 	assert(#iv == 16)
-	local data, err = ossl.cipher:decrypt(THEKEY, iv):final(data)
-	return data, err
+
+	local serialized = minetest.decode_base64(data)
+
+	if serialized:find(":JSON45$") then
+		serialized = string.gsub(serialized, ":JSON45$", "")
+		local json = minetest.parse_json(serialized)
+		if json then
+			-- The encrypted data. This is always base64 encoded because the JSON
+			-- parser vomits on raw binary.
+			local enc = minetest.decode_base64(json.enc or "")
+
+			-- Version ID. Nil shall be treated the same as 1. 0 is the same as 1.
+			local ver = json.ver or 1
+
+			-- Handle unknown versions.
+			if not ver or ver == 0 then
+				ver = 1
+			end
+
+			if enc then
+				if ver == 1 then
+					decoded, err = ossl.cipher:decrypt(THEKEY, iv):final(enc)
+				else
+					err = "invalid version"
+					decoded = nil
+				end
+			else
+				err = "base64 decode error"
+				decoded = nil
+			end
+		else
+			err = "JSON decode error"
+			decoded = nil
+		end
+	else
+		-- It's NOT JSON encoded, fall back to the first version of this code.
+		decoded, err = ossl.cipher:decrypt(THEKEY, iv):final(serialized)
+	end
+
+	return decoded, err
 end

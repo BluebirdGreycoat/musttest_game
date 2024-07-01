44a20f259fe21185fbf1f24a3e2d15ac6cdc42ef
BlueBird51 <bluebird.greycoat@gmail.com>
2024-06-30 20:00:06 -0500

    Use b64 for safety. Totally overlook b1

--- mods/ossl/api.lua
+++ mods/ossl/api.lua
@@ -6,24 +6,30 @@ local THEKEY = ossl.default_key
 ossl.default_key = nil
 assert(THEKEY and #THEKEY == 16)
 
--- Generate a random IV.
+-- Generate a random IV. Returns base64 version for safer handling.
 function ossl.geniv()
 	local s = ossl.randlib.bytes(16)
 	assert(#s == 16)
-	return s
+	return minetest.encode_base64(s)
 end
 
 -- Input IV + plaintext, get encrypted binary; or nil + errormsg.
 -- The IV does not need to be secret and should be stored with the binary.
 function ossl.encrypt(iv, text)
+	iv = minetest.decode_base64(iv)
 	assert(#iv == 16)
 	local data, err = ossl.cipher:encrypt(THEKEY, iv):final(text)
+	if data then
+		data = minetest.encode_base64(data)
+	end
 	return data, err
 end
 
 -- Input IV + encrypted binary, get plaintext; or nil + errormsg.
 -- The IV does not need to be secret and should live next to the binary.
 function ossl.decrypt(iv, data)
+	iv = minetest.decode_base64(iv)
+	data = minetest.decode_base64(data)
 	assert(#iv == 16)
 	local data, err = ossl.cipher:decrypt(THEKEY, iv):final(data)
 	return data, err

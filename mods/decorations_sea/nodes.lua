0146bfa858fa63958aad2e28d2d3f733e78449ea
BlueBird51 <bluebird.greycoat@gmail.com>
2024-07-15 07:37:05 -0500

    ... And another one in the can!

--- mods/decorations_sea/nodes.lua
+++ mods/decorations_sea/nodes.lua
@@ -83,7 +83,7 @@ local node_coral_info = {
 	{name="Purple Staghorn"},
 	{name="Oxblood Coral"},
 	{name="Yellow Emerald Coral"},
-	{name="Red Vanahorn"},
+	{name="Red Vanhorn"},
 	{name="Green Polyps"},
 }
 for k = 1, 8 do
@@ -96,5 +96,93 @@ for k = 1, 8 do
 		drop = "default:coral_skeleton",
 		silverpick_drop = true,
 		sounds = default.node_sound_stone_defaults(),
+	})
+end
+
+
+
+local function get_deco_place_func(data)
+	local function deco_on_place(itemstack, placer, pointed_thing)
+		-- Call on_rightclick if the pointed node defines it
+		if pointed_thing.type == "node" and placer and not placer:get_player_control().sneak then
+			local node_ptu = minetest.get_node(pointed_thing.under)
+			local def_ptu = minetest.registered_nodes[node_ptu.name]
+			if def_ptu and def_ptu.on_rightclick then
+				return def_ptu.on_rightclick(pointed_thing.under, node_ptu, placer, itemstack, pointed_thing)
+			end
+		end
+
+		local pos = pointed_thing.under
+		if minetest.get_node(pos).name ~= "default:sand" then
+			return itemstack
+		end
+
+		local height = math.random(data.min_height, data.max_height)
+		local pos_top = {x = pos.x, y = pos.y + height, z = pos.z}
+		local node_top = minetest.get_node(pos_top)
+		local def_top = minetest.registered_nodes[node_top.name]
+		local player_name = placer:get_player_name()
+
+		if def_top and def_top.liquidtype == "source" and minetest.get_item_group(node_top.name, "water") > 0 then
+			if not minetest.test_protection(pos, player_name) and not minetest.test_protection(pos_top, player_name) then
+				minetest.set_node(pos, {name = data.nodename, param2 = height * 16})
+				itemstack:take_item()
+			end
+		end
+
+		return itemstack
+	end
+
+	return deco_on_place
+end
+
+
+
+local sand_plant_info = {
+	{name="Giant Kelp", min_height=3, max_height=14},
+	{name="Red Dulse", min_height=3, max_height=6},
+	{name="Sea Lettuce", min_height=3, max_height=6},
+	{name="Sea Grass", min_height=1, max_height=1},
+	{name="Sea Fern", min_height=1, max_height=1},
+	{name="Seaweed", min_height=1, max_height=1},
+}
+for k = 1, 6 do
+	local tex = "decorations_sea_seagrass_0" .. k .. ".png"
+	local nodename = "decorations_sea:sand_with_seagrass_" .. k
+
+	minetest.register_node(nodename, {
+		description = sand_plant_info[k].name,
+		drawtype = "plantlike_rooted",
+		waving = 1,
+		tiles = {"default_sand.png"},
+		special_tiles = {{name = tex, tileable_vertical = true}},
+		inventory_image = tex,
+		wield_image = tex,
+		paramtype = "light",
+		paramtype2 = "leveled",
+		groups = utility.dig_groups("plant", {flammable = 2}),
+		selection_box = {
+			type = "fixed",
+			fixed = {
+					{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+					{-2/16, 0.5, -2/16, 2/16, (1.0 * sand_plant_info[k].min_height + 0.5), 2/16},
+			},
+		},
+		node_dig_prediction = "default:sand",
+		node_placement_prediction = "",
+		sounds = default.node_sound_sand_defaults({
+			dig = {name = "default_dig_snappy", gain = 0.2},
+			dug = {name = "default_grass_footstep", gain = 0.25},
+		}),
+
+		on_place = get_deco_place_func({
+			min_height = sand_plant_info[k].min_height,
+			max_height = sand_plant_info[k].max_height,
+			nodename = nodename,
+		}),
+
+		after_destruct  = function(pos, oldnode)
+			minetest.set_node(pos, {name = "default:sand"})
+		end
 	})
 end

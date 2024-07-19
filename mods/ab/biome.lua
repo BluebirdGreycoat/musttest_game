eadd8f3e1549b2619569caf991d136bafc3c4cdd
BlueBird51 <bluebird.greycoat@gmail.com>
2024-07-19 18:17:48 -0500

    The sheer incompentence over at the USSS is so mindboggling it exceeds the insane and enters fully into conspiracy territory

--- mods/ab/biome.lua
+++ mods/ab/biome.lua
@@ -36,6 +36,7 @@ function ab.generate_biome(vm, minp, maxp, seed, ystart, yend, heightfunc)
 	local trees = {}
 	local deadtrees = {}
 	local glowstones = {}
+	local raretrees = {}
 
 	local glowstones_count = pr:next(1, 5)
 	for k = 1, glowstones_count do
@@ -51,6 +52,33 @@ function ab.generate_biome(vm, minp, maxp, seed, ystart, yend, heightfunc)
 			x = x0 + pr:next(0, (x1 - x0)),
 			z = z0 + pr:next(0, (z1 - z0)),
 		}
+	end
+
+	local grass_count = 1066
+	for k = 1, grass_count do
+		grass[#grass + 1] = {
+			x = x0 + pr:next(0, (x1 - x0)),
+			z = z0 + pr:next(0, (z1 - z0)),
+		}
+	end
+
+	local trees_count = 32
+	for k = 1, trees_count do
+		trees[#trees + 1] = {
+			x = x0 + pr:next(0, (x1 - x0)),
+			z = z0 + pr:next(0, (z1 - z0)),
+		}
+	end
+
+	-- Very rarely, alive trees on the surface.
+	if pr:next(1, 1) == 1 then
+		local raretrees_count = pr:next(1, 2)
+		for k = 1, raretrees_count do
+			raretrees[#raretrees + 1] = {
+				x = x0 + pr:next(0, (x1 - x0)),
+				z = z0 + pr:next(0, (z1 - z0)),
+			}
+		end
 	end
 
 	for z = z0, z1 do
@@ -71,22 +99,6 @@ function ab.generate_biome(vm, minp, maxp, seed, ystart, yend, heightfunc)
 						if y == ground_y then
 							vm_data[vp_c] = c_cobble
 						end
-
-						-- Surface decorations here.
-						if y >= ground_y then
-							local grassed = false
-							if pr:next(1, 6) == 1 then
-								grass[#grass + 1] = {x=x, y=y+1, z=z}
-								grassed = true
-							end
-
-							-- Place trees when canyons are at least this deep.
-							if canyon_offset < -50 and not grassed then
-								if pr:next(1, 200) == 1 then
-									trees[#trees + 1] = {x=x, y=y, z=z}
-								end
-							end
-						end
 					end
 				end
 			end
@@ -95,17 +107,62 @@ function ab.generate_biome(vm, minp, maxp, seed, ystart, yend, heightfunc)
 
 	vm:set_data(vm_data)
 
-  for k = 1, #trees do
-		ab.place_acacia_tree(vm, trees[k])
-  end
+  local function place_plains_decorations(decolist, decofunc)
+		for k = 1, #decolist do
+			local x = decolist[k].x
+			local z = decolist[k].z
+			for y = y1, y0, -1 do
+				if y >= ystart and y <= yend then
+					local vp_c = area:index(x, y, z)
+					local vp_u = area:index(x, y + 1, z)
+					local ground_y, canyon_offset = heightfunc(x, y, z)
 
-  for k = 1, #grass do
-		if pr:next(1, 7) == 1 then
-			minetest.set_node(grass[k], {name="default:dry_shrub"})
-		else
-			minetest.set_node(grass[k], {name="default:dry_grass2_" .. pr:next(1, 5), param2=2})
+					local cid_c = vm_data[vp_c]
+					local cid_u = vm_data[vp_u]
+
+					if cid_c == c_cobble and cid_u == c_air then
+						if y >= ground_y and canyon_offset == 0 then
+							-- On surface, not in canyon or mesa.
+							local p = {x=x, y=y, z=z}
+							decofunc(p)
+
+							-- Goto next item.
+							break
+						end
+					end
+				end
+			end
 		end
-  end
+	end
+
+  local function place_canyon_decorations(decolist, depth, decofunc)
+		depth = -(abs(depth))
+		for k = 1, #decolist do
+			local x = decolist[k].x
+			local z = decolist[k].z
+			for y = y1, y0, -1 do
+				if y >= ystart and y <= yend then
+					local vp_c = area:index(x, y, z)
+					local vp_u = area:index(x, y + 1, z)
+					local ground_y, canyon_offset = heightfunc(x, y, z)
+
+					local cid_c = vm_data[vp_c]
+					local cid_u = vm_data[vp_u]
+
+					if cid_c == c_cobble and cid_u == c_air then
+						if y >= ground_y and canyon_offset < depth then
+							-- On surface, not in canyon or mesa.
+							local p = {x=x, y=y, z=z}
+							decofunc(p)
+
+							-- Goto next item.
+							break
+						end
+					end
+				end
+			end
+		end
+	end
 
   local function place_ground_decorations(decolist, decofunc)
 		for k = 1, #decolist do
@@ -121,7 +178,7 @@ function ab.generate_biome(vm, minp, maxp, seed, ystart, yend, heightfunc)
 					local cid_u = vm_data[vp_u]
 
 					if cid_c == c_cobble and cid_u == c_air then
-						if y >= ground_y and canyon_offset == 0 then
+						if y >= ground_y then
 							-- On surface, not in canyon or mesa.
 							local p = {x=x, y=y, z=z}
 							decofunc(p)
@@ -156,9 +213,27 @@ function ab.generate_biome(vm, minp, maxp, seed, ystart, yend, heightfunc)
 		end
   end
 
-	place_ground_decorations(glowstones, function(pos)
+  local function put_grass(pos)
+		local p = vector.offset(pos, 0, 1, 0)
+		if pr:next(1, 7) == 1 then
+			minetest.set_node(p, {name="default:dry_shrub"})
+		else
+			minetest.set_node(p, {name="default:dry_grass2_" .. pr:next(1, 5), param2=2})
+		end
+	end
+
+	place_plains_decorations(glowstones, function(pos)
 		minetest.set_node(pos, {name="glowstone:minerals"})
 	end)
 
-	place_ground_decorations(deadtrees, put_deadtree)
+	place_ground_decorations(grass, put_grass)
+	place_plains_decorations(deadtrees, put_deadtree)
+
+	place_canyon_decorations(trees, 50, function(pos)
+		ab.place_acacia_tree(vm, pos)
+	end)
+
+	place_plains_decorations(raretrees, function(pos)
+		ab.place_acacia_tree(vm, pos)
+	end)
 end

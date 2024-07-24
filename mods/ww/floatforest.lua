a84fe913afab2f643c2506eb7b90a8309a6b4310
BlueBird51 <bluebird.greycoat@gmail.com>
2024-07-23 21:31:47 -0500

    Floating logmats

--- mods/ww/floatforest.lua
+++ mods/ww/floatforest.lua
@@ -1,10 +1,12 @@
 
 local vm_data = {}
+local vm_param2_data = {}
 local c_air = minetest.get_content_id("air")
 local c_ignore = minetest.get_content_id("ignore")
 local c_bedrock = minetest.get_content_id("bedrock:bedrock")
-local c_sand = minetest.get_content_id("default:sand")
+local c_lily = minetest.get_content_id("flowers:waterlily")
 
+local NN_LOGMAT = "basictrees:jungletree_cube"
 local TAN_OF_1 = math.tan(1)
 
 local abs = math.abs
@@ -12,6 +14,10 @@ local floor = math.floor
 local max = math.max
 local min = math.min
 local tan = math.tan
+
+-- Param2 horizontal branch rotations.
+local branch_rotations = {4, 8, 12, 16}
+local branch_directions = {2, 0, 3, 1}
 
 ww.create_2d_noise("forestpattern", {
 	offset = 0,
@@ -30,6 +36,7 @@ function ww.generate_floating_forests(vm, minp, maxp, seed, ystart, yend, ygroun
 	local pr = PcgRandom(seed + 11223)
 
 	vm:get_data(vm_data)
+	vm:get_param2_data(vm_param2_data)
 
 	local x1 = maxp.x
 	local y1 = maxp.y
@@ -37,6 +44,8 @@ function ww.generate_floating_forests(vm, minp, maxp, seed, ystart, yend, ygroun
 	local x0 = minp.x
 	local y0 = minp.y
 	local z0 = minp.z
+
+	local logmats = {}
 
 	-- Compute side lengths.
 	-- Note: noise maps use overgeneration coordinates/sizes.
@@ -52,6 +61,7 @@ function ww.generate_floating_forests(vm, minp, maxp, seed, ystart, yend, ygroun
 	local forestpattern = ww.get_2d_noise(bp2d, sides2D, "forestpattern")
 	local min_noise = 0
 	local max_noise = 0
+	local ocean_surface = yground + 1
 
 	for z = z0, z1 do
 		for x = x0, x1 do
@@ -61,9 +71,28 @@ function ww.generate_floating_forests(vm, minp, maxp, seed, ystart, yend, ygroun
 					local vp = area:index(x, y, z)
 					local forest = forestpattern[n2d]
 
-					if y == yground then
+					if y == ocean_surface then
 						if forest > 0.5 then
-							vm_data[vp] = c_sand
+							if pr:next(1, 6) == 1 then
+								vm_data[vp] = c_lily
+								vm_param2_data[vp] = pr:next(0, 3)
+							end
+
+							if pr:next(1, 10) == 1 then
+								logmats[#logmats + 1] = {x=x, y=y, z=z}
+							end
+
+							if pr:next(1, 16) == 1 then
+								logmats[#logmats + 1] = {x=x, y=y-1, z=z}
+							end
+
+							if pr:next(1, 16) == 1 then
+								logmats[#logmats + 1] = {x=x, y=y-2, z=z}
+							end
+
+							if pr:next(1, 30) == 1 then
+								logmats[#logmats + 1] = {x=x, y=y-4, z=z}
+							end
 						end
 					end
 				end
@@ -73,4 +102,30 @@ function ww.generate_floating_forests(vm, minp, maxp, seed, ystart, yend, ygroun
 
 	--print(min_noise, max_noise)
 	vm:set_data(vm_data)
+	vm:set_param2_data(vm_param2_data)
+
+	local function put_logmat(pos)
+		local p = vector.offset(pos, 0, -1, 0)
+		local n = pr:next(2, 8)
+
+		if pr:next(1, 6) == 1 then
+			for i = 1, n do
+				minetest.set_node(p, {name=NN_LOGMAT})
+				p = vector.offset(p, 0, -1, 0)
+			end
+		else
+			local diridx = pr:next(1, 4)
+			local facedir = branch_directions[diridx]
+			local vec = minetest.facedir_to_dir(facedir)
+
+			for i = 1, n do
+				minetest.set_node(p, {name=NN_LOGMAT, param2=branch_rotations[diridx]})
+				p = vector.add(p, vec)
+			end
+		end
+  end
+
+  for k = 1, #logmats do
+		put_logmat(logmats[k])
+  end
 end

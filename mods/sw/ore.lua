afe9a660bf472f034fcded2d1164be76de020071
BlueBird51 <bluebird.greycoat@gmail.com>
2024-08-31 12:41:15 -0500

    The Bible is the best antidote to Determinism/Decreetalism/Fatalism. Read in context. Don't prooftext!

--- mods/sw/ore.lua
+++ mods/sw/ore.lua
@@ -1,4 +1,7 @@
 
+local REALM_START = 10150
+local REALM_END = 15150
+
 local sheet_ores = {
 	{ore="morerocks:marble", seed=1, threshhold=0.3},
 	{ore="morerocks:serpentine", seed=2, threshhold=0.4},
@@ -16,8 +19,8 @@ for index, data in ipairs(sheet_ores) do
 		column_height_min = 4,
 		column_height_max = 10,
 		column_midpoint_factor = 0.5,
-		y_min = 10150,
-		y_max = 15150,
+		y_min = REALM_START,
+		y_max = REALM_END,
 		noise_threshhold = 0.8 + data.threshhold,
 		noise_params = {
 			offset = 0,
@@ -26,6 +29,49 @@ for index, data in ipairs(sheet_ores) do
 			seed = SHEET_ORE_SEED_FLOOR + data.seed,
 			octaves = 2,
 			persist = 0.8,
+		}
+	})
+end
+
+
+
+local scatter_ores = {
+	-- Specials.
+	{ore="default:mossycobble", seed=298, threshhold=-0.3, scarcity=8, count=8, size=3, wherein="default:cobble"},
+	{ore="cavestuff:dark_obsidian", seed=11, threshhold=-0.3, scarcity=8, count=8, size=3, wherein="default:obsidian"},
+
+	-- Commons.
+	{ore="default:stone_with_coal", seed=1, threshhold=0.1, scarcity=8, count=8, size=3},
+	{ore="default:stone_with_iron", seed=2, threshhold=0.2, scarcity=7, count=5, size=3},
+	{ore="default:stone_with_copper", seed=3, threshhold=0.2, scarcity=9, count=5, size=3},
+	{ore="quartz:quartz_ore", seed=30, threshhold=0.1, scarcity=9, count=5, size=3},
+
+	-- Rares.
+	{ore="default:stone_with_gold", seed=4, threshhold=0.4, scarcity=13, count=5, size=3},
+	{ore="default:stone_with_mese", seed=5, threshhold=0.4, scarcity=14, count=5, size=3},
+	{ore="default:stone_with_diamond", seed=6, threshhold=0.4, scarcity=15, count=4, size=3},
+	{ore="default:mese", seed=6, threshhold=0.5, scarcity=36, count=4, size=2},
+}
+local SCATTER_ORE_SEED_FLOOR = 2818
+
+for index, data in ipairs(scatter_ores) do
+	minetest.register_ore({
+		ore_type = "scatter",
+		ore = data.ore,
+		wherein = data.wherein or {"default:stone", "default:cobble"},
+		y_min = REALM_START,
+		y_max = REALM_END,
+    clust_scarcity = data.scarcity * data.scarcity * data.scarcity,
+    clust_num_ores = data.count,
+    clust_size     = data.size,
+		noise_threshhold = -0.1 + data.threshhold,
+		noise_params = {
+			offset = 0,
+			scale = 1,
+			spread = {x=500, y=500, z=500},
+			seed = SCATTER_ORE_SEED_FLOOR + data.seed,
+			octaves = 4,
+			persist = 0.5,
 		}
 	})
 end

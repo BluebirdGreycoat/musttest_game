8aeb9f3732eb72b69c0556226de055689127df37
BlueBird51 <bluebird.greycoat@gmail.com>
2024-09-28 12:17:12 -0500

    Sheriff of Notingham's got to get his taxes

--- mods/wizard/init.lua
+++ mods/wizard/init.lua
@@ -174,12 +174,19 @@ function wizard.banish_staff(itemstack, user, pt)
 	local text = meta:get_string("text"):trim()
 	local author = meta:get_string("author")
 
+	local tokens = text:split(":")
+	if #tokens == 0 or #tokens > 2 then
+		return
+	end
+
+	local target_name = (tokens[2] or tokens[1]):trim()
+
 	-- Staff user must be sign author.
 	if author ~= pname then
 		return
 	end
 
-	local ptarget = minetest.get_player_by_name(text)
+	local ptarget = minetest.get_player_by_name(target_name)
 	if not ptarget or not ptarget:is_player() then
 		return
 	end
@@ -201,11 +208,37 @@ function wizard.banish_staff(itemstack, user, pt)
 		return itemstack
 	end
 
+	-- Staff user can specify an origin other than himself.
+	-- The wizard may use a minion to extend his power.
+	local origin = user:get_pos()
+	if tokens[1] and tokens[2] then
+		local origin_name = tokens[1]:trim()
+		if origin_name ~= target_name then
+			local origin_ref = minetest.get_player_by_name(origin_name)
+			if origin_ref then
+				origin = origin_ref:get_pos()
+			end
+		end
+	end
+
+	-- Range limit.
+	if vector.distance(origin, ptarget:get_pos()) > 100 then
+		return
+	end
+
 	-- Perform kick action AFTER returning from the current stack frame.
 	-- User might kick self.
 	local ntarget = ptarget:get_player_name()
 	minetest.after(0, function()
 		minetest.kick_player(ntarget, "Momentarily banished.")
+	end)
+
+	-- Take 15 hp of health from the wizard.
+	minetest.after(0, function()
+		local pref = minetest.get_player_by_name(pname)
+		if pref then
+			utility.damage_player(pref, "fleshy", 15 * 500)
+		end
 	end)
 end
 

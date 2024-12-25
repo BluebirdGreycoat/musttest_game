27750c3efdf7318a99da95e45fd4442777666668
MyCatAteMyCode <FeralAttackKitten@proton.me>
2024-12-25 18:50:52 -0500

    More Flora for Xen

---
+++ mods/vines/glowvine.lua
@@ -0,0 +1,36 @@
+
+-- New Vines for Xen
+
+dofile(vines.modpath .. "/glowvine.lua")
+
+ minetest.register_node('vine:luminoustreevine', {
+	 drawtype = "plantlike",
+	 inventory_image = "dvd_luminousvineend.png",
+	 weild_image = "dvd_luminousvine.png",
+	 description = 'Twilight Vine End',
+	 tiles = {'dvd_luminousvineend.png'},
+	 paramtype = "light",
+	 sunlight_propagates = true, 
+	 light_source = 6,
+	 paramtype2 = "wallmounted",
+	 is_ground_content = false, 
+	 walkable = false,
+	 climbable = true,
+	 groups = {level = 1, snappy=3, oddly_breakable_by_hand=1},
+ })
+ 
+  minetest.register_node('vine:luminoustreevineend', {
+	 drawtype = "plantlike",
+	 inventory_image = "dvd_luminousvine.png",
+	 weild_image = "dvd_luminousvine.png",
+	 description = 'Twilight Vine',
+	 tiles = {'dvd_luminousvine.png'},
+	 paramtype = "light",
+	 sunlight_propagates = true, 
+	 light_source = 6,
+	 paramtype2 = "wallmounted",
+	 is_ground_content = false, 
+	 walkable = false,
+	 climbable = true,
+	 groups = {level = 1, snappy=3, oddly_breakable_by_hand=1},
+ })

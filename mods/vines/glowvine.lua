c97177dfd97741f2510cccec744671f98e5b93f6
MyCatAteMyCode <FeralAttackKitten@proton.me>
2024-12-25 19:15:04 -0500

    Some Changes

--- mods/vines/glowvine.lua
+++ mods/vines/glowvine.lua
@@ -9,16 +9,24 @@
 	 weild_image = "dvd_luminousvine.png",
 	 description = 'Twilight Vine',
 	 tiles = {'dvd_luminousvineend.png'},
-	 paramtype = "light",
-	 sunlight_propagates = true, 
+	 paramtype = "light", 
 	 light_source = 6,
-	 paramtype2 = "wallmounted",
+	 groups = utility.dig_groups("plant", {
+	 hanging_node = 1, flammable = 3,
+	 }),
 	 is_ground_content = false, 
 	 walkable = false,
 	 climbable = true,
+	 drop = "",
+	 shears_drop = true,
 	 movement_speed_multiplier = default.SLOW_SPEED_PLANTS,
 	 sounds = default.node_sound_leaves_defaults(),
 	 groups = {level = 1, snappy=3, oddly_breakable_by_hand=1},
+	 selection_box = {
+     type = "fixed",
+     fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
+  },
+})
  })
  
   minetest.register_node('vine:luminoustreevineend', {
@@ -28,13 +36,20 @@
 	 description = 'Twilight Vine',
 	 tiles = {'dvd_luminousvine.png'},
 	 paramtype = "light",
-	 sunlight_propagates = true, 
 	 light_source = 6,
-	 paramtype2 = "wallmounted",
+	 groups = utility.dig_groups("plant", {
+     hanging_node = 1, flammable = 3,
+     }),
 	 is_ground_content = false, 
 	 walkable = false,
 	 climbable = true,
+	 drop = "",
+	 shears_drop = true,
 	 movement_speed_multiplier = default.SLOW_SPEED_PLANTS,
 	 sounds = default.node_sound_leaves_defaults(),
 	 groups = {level = 1, snappy=3, oddly_breakable_by_hand=1},
+	 selection_box = {
+     type = "fixed",
+     fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
+  },
  })

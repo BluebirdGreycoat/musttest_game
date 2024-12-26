4b37ac5c8e9b9b73a8906e1b0dee5d722a23657a
BlueBird51 <bluebird.greycoat@gmail.com>
2024-12-25 18:43:33 -0600

    Fix DVD's indentation (fix panic in others lol)

--- mods/vines/glowvine.lua
+++ mods/vines/glowvine.lua
@@ -1,54 +1,53 @@
 
 -- New Vines for Xen
 
+local function vine_selectionbox()
+	return {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
+	}
+end
 
+minetest.register_node('vines:luminoustreevine', {
+	drawtype = "plantlike",
+	inventory_image = "dvd_luminousvineend.png",
+	weild_image = "dvd_luminousvine.png",
+	description = 'Twilight Vine',
+	tiles = {'dvd_luminousvineend.png'},
+	paramtype = "light",
+	light_source = 6,
+	groups = utility.dig_groups("plant", {
+		hanging_node = 1, flammable = 3,
+	}),
+	is_ground_content = false,
+	walkable = false,
+	climbable = true,
+	drop = "",
+	shears_drop = true,
+	movement_speed_multiplier = default.SLOW_SPEED_PLANTS,
+	sounds = default.node_sound_leaves_defaults(),
+	groups = {level = 1, snappy=3, oddly_breakable_by_hand=1},
+	selection_box = vine_selectionbox(),
+})
 
- minetest.register_node('vines:luminoustreevine', {
-	 drawtype = "plantlike",
-	 inventory_image = "dvd_luminousvineend.png",
-	 weild_image = "dvd_luminousvine.png",
-	 description = 'Twilight Vine',
-	 tiles = {'dvd_luminousvineend.png'},
-	 paramtype = "light", 
-	 light_source = 6,
-	 groups = utility.dig_groups("plant", {
-	 hanging_node = 1, flammable = 3,
-	 }),
-	 is_ground_content = false, 
-	 walkable = false,
-	 climbable = true,
-	 drop = "",
-	 shears_drop = true,
-	 movement_speed_multiplier = default.SLOW_SPEED_PLANTS,
-	 sounds = default.node_sound_leaves_defaults(),
-	 groups = {level = 1, snappy=3, oddly_breakable_by_hand=1},
-	 selection_box = {
-     type = "fixed",
-     fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
-  },
- })
- 
-  minetest.register_node('vines:luminoustreevineend', {
-	 drawtype = "plantlike",
-	 inventory_image = "dvd_luminousvine.png",
-	 weild_image = "dvd_luminousvine.png",
-	 description = 'Twilight Vine',
-	 tiles = {'dvd_luminousvine.png'},
-	 paramtype = "light",
-	 light_source = 6,
-	 groups = utility.dig_groups("plant", {
-     hanging_node = 1, flammable = 3,
-     }),
-	 is_ground_content = false, 
-	 walkable = false,
-	 climbable = true,
-	 drop = "",
-	 shears_drop = true,
-	 movement_speed_multiplier = default.SLOW_SPEED_PLANTS,
-	 sounds = default.node_sound_leaves_defaults(),
-	 groups = {level = 1, snappy=3, oddly_breakable_by_hand=1},
-	 selection_box = {
-     type = "fixed",
-     fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
-  },
- })
+minetest.register_node('vines:luminoustreevineend', {
+	drawtype = "plantlike",
+	inventory_image = "dvd_luminousvine.png",
+	weild_image = "dvd_luminousvine.png",
+	description = 'Twilight Vine',
+	tiles = {'dvd_luminousvine.png'},
+	paramtype = "light",
+	light_source = 6,
+	groups = utility.dig_groups("plant", {
+		hanging_node = 1, flammable = 3,
+	}),
+	is_ground_content = false,
+	walkable = false,
+	climbable = true,
+	drop = "",
+	shears_drop = true,
+	movement_speed_multiplier = default.SLOW_SPEED_PLANTS,
+	sounds = default.node_sound_leaves_defaults(),
+	groups = {level = 1, snappy=3, oddly_breakable_by_hand=1},
+	selection_box = vine_selectionbox(),
+})

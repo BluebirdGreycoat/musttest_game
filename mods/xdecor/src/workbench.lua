bd43ea2e6da27b05f682607a22e5ebd9cdc4c3f5
BlueBird51 <bluebird.greycoat@gmail.com>
2024-12-27 17:02:49 -0600

    Workbench need testing, DVD

--- mods/xdecor/src/workbench.lua
+++ mods/xdecor/src/workbench.lua
@@ -1,13 +1,63 @@
 
+-- Nodes
+
 xdecor.register("workbench", {
-	description = "Woodworking Bench (Decorative)",
+	description = "Workbench",
 	groups = utility.dig_groups("furniture"),
 	sounds = default.node_sound_wood_defaults(),
-	tiles = {"xdecor_workbench_top.png",   "xdecor_workbench_top.png",
-		 "xdecor_workbench_sides.png", "xdecor_workbench_sides.png",
-		 "xdecor_workbench_front.png", "xdecor_workbench_front.png"},
+	tiles = {
+		"xdecor_workbench_top.png",
+		"xdecor_workbench_top.png",
+		"xdecor_workbench_sides.png",
+		"xdecor_workbench_sides.png",
+		"xdecor_workbench_front.png",
+		"xdecor_workbench_front.png",
+	},
 	on_rotate = screwdriver.rotate_simple,
 	nostairs = true,
+
+	-- Behave like tool, not a building node.
+	stack_max = 1,
+
+	after_place_node = function(...)
+		return workbench.after_place_node(...)
+	end,
+
+	on_rightclick = function(...)
+		return workbench.on_rightclick(...)
+	end,
+
+	can_dig = function(...)
+		return workbench.can_dig(...)
+	end,
+
+	on_blast = function(...)
+		return workbench.on_blast(...)
+	end,
+
+	allow_metadata_inventory_move = function(...)
+		return workbench.allow_metadata_inventory_move(...)
+	end,
+
+	allow_metadata_inventory_put = function(...)
+		return workbench.allow_metadata_inventory_put(...)
+	end,
+
+	allow_metadata_inventory_take = function(...)
+		return workbench.allow_metadata_inventory_take(...)
+	end,
+
+	on_metadata_inventory_move = function(...)
+		return workbench.on_metadata_inventory_move(...)
+	end,
+
+	on_metadata_inventory_put = function(...)
+		return workbench.on_metadata_inventory_put(...)
+	end,
+
+	on_metadata_inventory_take = function(...)
+		return workbench.on_metadata_inventory_take(...)
+	end,
 })
 
 -- Craft items

693c0902826afd627ee9813310dd7018cea60ea9
MyCatAteMyCode <FeralAttackKitten@proton.me>
2024-12-27 23:41:46 -0500

    Oops, a bit too common.

--- mods/sw/ore.lua
+++ mods/sw/ore.lua
@@ -78,10 +78,10 @@ local scatter_ores = {
 	{ore="cavestuff:glow_obsidian", seed=3312, threshhold=-0.5, scarcity=10, count=3, size=5, y_min=XEN_BEGIN, y_max=XEN_END, wherein={"sw:teststone1", "sw:teststone2"}, octaves=2},
 	
 	--Ir'xen gems.
-	{ore="gems:ruby2_ore", seed=772, threshhold=-0.5, scarcity=16, count=12, size=6, y_min=XEN_UPPERMID, y_max=XEN_END, wherein={"sw:teststone1",}, octaves=2},
-	{ore="gems:emerald2_ore", seed=772, threshhold=-0.5, scarcity=16, count=12, size=6, y_min=XEN_BEGIN, y_max=XEN_UPPERMID, wherein={"sw:teststone1",}, octaves=2},
-	{ore="gems:sapphire2_ore", seed=772, threshhold=-0.5, scarcity=16, count=12, size=6, y_min=XEN_UPPERMID, y_max=XEN_END, wherein={"sw:teststone1",}, octaves=2},
-	{ore="gems:amethyst2_ore", seed=772, threshhold=-0.5, scarcity=16, count=12, size=6, y_min=XEN_BEGIN, y_max=XEN_UPPERMID, wherein={"sw:teststone1",}, octaves=2},
+	{ore="gems:ruby2_ore", seed=772, threshhold=-0.5, scarcity=25, count=12, size=6, y_min=XEN_UPPERMID, y_max=XEN_END, wherein={"sw:teststone1",}, octaves=2},
+	{ore="gems:emerald2_ore", seed=772, threshhold=-0.5, scarcity=25, count=12, size=6, y_min=XEN_BEGIN, y_max=XEN_UPPERMID, wherein={"sw:teststone1",}, octaves=2},
+	{ore="gems:sapphire2_ore", seed=772, threshhold=-0.5, scarcity=25, count=12, size=6, y_min=XEN_UPPERMID, y_max=XEN_END, wherein={"sw:teststone1",}, octaves=2},
+	{ore="gems:amethyst2_ore", seed=772, threshhold=-0.5, scarcity=25, count=12, size=6, y_min=XEN_BEGIN, y_max=XEN_UPPERMID, wherein={"sw:teststone1",}, octaves=2},
 
 	
 }

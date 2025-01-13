9cec9c8cdb7b8b1595dfaa3c0f442991e1fa5a6a
BlueBird51 <bluebird.greycoat@gmail.com>
2025-01-12 22:00:36 -0600

    Update /whois chatcommand

--- mods/network_whois/init.lua
+++ mods/network_whois/init.lua
@@ -10,7 +10,17 @@ local function sanitize_ipv4(ip)
     return ip
 end
 
-function network_whois.display(name, target)
+local function ip2str(ver)
+	if ver == 4 then
+		return "IPv4"
+	elseif ver == 6 then
+		return "IPv6"
+	else
+		return tostring(ver)
+	end
+end
+
+function network_whois.display(name, target, formatting)
 	local player = minetest.get_player_by_name(target)
 	if not player then
 		minetest.chat_send_player(name, "# Server: Player <" .. rename.gpn(target) .. "> not found.")
@@ -23,14 +33,34 @@ function network_whois.display(name, target)
 		return
 	end
 
-	minetest.chat_send_player(name, "# Server: Account <" .. rename.gpn(target) ..
-		">: ADR " .. sanitize_ipv4(info.address) ..
-		", IPV " .. info.ip_version ..
-		", CU " .. info.connection_uptime ..
-		", AVG RTT " .. info.avg_rtt ..
-		", PV " .. info.protocol_version ..
-		", RN <" .. rename.grn(target) .. ">" ..
-		".")
+	if formatting == "table" then
+		local tb = {
+			"IP Address:        " .. sanitize_ipv4(info.address),
+			"IP Version:        " .. ip2str(info.ip_version),
+			"Connection Uptime: " .. info.connection_uptime,
+			"Avg RTT:           " .. info.avg_rtt or "N/A",
+			"Min RTT:           " .. info.min_rtt or "N/A",
+			"Max RTT:           " .. info.max_rtt or "N/A",
+			"Protocol Version:  " .. info.protocol_version,
+			"Formspec Version:  " .. info.formspec_version,
+			"Language Code:     " .. info.lang_code or "N/A",
+			"Login Name:        " .. rename.grn(target),
+		}
+
+		minetest.chat_send_player(name, "# Server: WHOIS data for account <" .. rename.gpn(target) .. ">:")
+		for k, v in ipairs(tb) do
+			minetest.chat_send_player(name, "# Server:     " .. v)
+		end
+	else
+		minetest.chat_send_player(name, "# Server: Account <" .. rename.gpn(target) ..
+			">: ADR " .. sanitize_ipv4(info.address) ..
+			", IPV " .. info.ip_version ..
+			", CU " .. info.connection_uptime ..
+			", AVG RTT " .. info.avg_rtt ..
+			", PV " .. info.protocol_version ..
+			", RN <" .. rename.grn(target) .. ">" ..
+			".")
+	end
 end
 
 
@@ -45,7 +75,7 @@ function network_whois.whois(name, param)
 		return true
 	end
 
-	network_whois.display(name, param)
+	network_whois.display(name, param, "table")
 	return true
 end
 

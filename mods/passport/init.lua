0269e1c27e2706e0dc0fc8bdedf11d44bb719173
BlueBird51 <bluebird.greycoat@gmail.com>
2026-07-05 19:13:56 -0500

    Autoleave newbies channel

--- mods/passport/init.lua
+++ mods/passport/init.lua
@@ -268,6 +268,7 @@ passport.on_use = function(itemstack, user, pointed)
 				minetest.chat_send_player(pname, "# Server: A newly initialized Key of Citizenship begins to emit a soft blue glow.")
 				shout.channel_handle_joinleave(pname, "citizens", true, true)
 				shout.channel_handle_joinleave(pname, "channels", true, true)
+				shout.channel_handle_joinleave(pname, "newbies", false, true)
 			end)
 
 			changed = true

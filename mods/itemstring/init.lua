d4948983c9c876c39101ae14606413ea2981e6e9
BlueBird51 <bluebird.greycoat@gmail.com>
2026-07-12 10:30:15 -0500

    This at least I KNOW is going to be useful

--- mods/itemstring/init.lua
+++ mods/itemstring/init.lua
@@ -2,22 +2,108 @@
 if not minetest.global_exists("itemstring") then itemstring = {} end
 itemstring.modpath = minetest.get_modpath("itemstring")
 
-minetest.register_privilege("item_info", {
-	description = "User can get wielded item info.",
-	give_to_singleplayer = false,
-})
+function itemstring.handle_command(pname, param)
+	local player = minetest.get_player_by_name(pname)
+	if not player then return end
+	if not player:is_player() then return end
 
-minetest.register_chatcommand("item-string", {
-	params = "",
-	description = "Get the item-string of a wielded item.",
-	privs = {item_info=true},
-
-	func = function(name, param)
-		local player = minetest.get_player_by_name(name)
-		if not player then return end
-		if not player:is_player() then return end
-		local info = player:get_wielded_item():to_string()
-		minetest.chat_send_player(name, "# Server: Wielded: '" .. info .. "'.")
-		return true
+	local stack = player:get_wielded_item()
+	if stack:is_empty() then
+		minetest.chat_send_player(pname, "# Server: Wielding nothing.")
+	else
+		local info = stack:to_string()
+		minetest.chat_send_player(pname, "# Server: Wielded: " .. info)
 	end
-})
+end
+
+function itemstring.handle_command_search(pname, param)
+	local player = minetest.get_player_by_name(pname)
+	if not player then return end
+	if not player:is_player() then return end
+
+	local function response(msg)
+		minetest.chat_send_player(pname, "# Server: " .. msg)
+	end
+
+	if #param == 0 then
+		local count = 0
+		for k, v in pairs(minetest.registered_items) do
+			count = count + 1
+		end
+
+		response("No search term(s) provided.")
+		response("There are " .. count .. " registered items.")
+		return
+	end
+
+	local matching = {}
+
+	for item, _ in pairs(minetest.registered_items) do
+		local tokens = param:split("[ _:]", false, -1, true)
+		local count = 0
+
+		for _, str in ipairs(tokens) do
+			if item:find(str, 1, true) then
+				count = count + 1
+			end
+		end
+
+		if count == #tokens then
+			table.insert(matching, item)
+		end
+	end
+
+	if #matching > 0 then
+		response("Found " .. #matching .. " items:")
+		local max_report = 0
+		local aborted = false
+
+		for _, str in ipairs(matching) do
+			response("    " .. str)
+			max_report = max_report + 1
+			if max_report > 20 then
+				aborted = true
+				break
+			end
+		end
+
+		if aborted then
+			response("    ... (not showing all items).")
+		end
+	else
+		response("Found no items matching search.")
+	end
+end
+
+if not itemstring.registered then
+	itemstring.registered = true
+
+	minetest.register_privilege("item_info", {
+		description = "User can get wielded item info.",
+		give_to_singleplayer = false,
+	})
+
+	minetest.register_chatcommand("item-string", {
+		params = "",
+		description = "Get the item-string of a wielded item.",
+		privs = {item_info=true},
+
+		func = function(...)
+			return itemstring.handle_command(...)
+		end
+	})
+
+	minetest.register_chatcommand("item-search", {
+		params = "<name>",
+		description = "Find item-strings of matching registered items.",
+		privs = {item_info=true},
+
+		func = function(...)
+			return itemstring.handle_command_search(...)
+		end
+	})
+
+	local c = "itemstring:core"
+	local f = itemstring.modpath .. "/init.lua"
+	reload.register_file(c, f, false)
+end

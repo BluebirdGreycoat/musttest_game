1c23a0d8eec97a46cb1e11c556934f58c79048cb
BlueBird51 <bluebird.greycoat@gmail.com>
2026-07-12 22:52:32 -0500

    Don't be a complete dweeb

--- mods/serveressentials/sethome.lua
+++ mods/serveressentials/sethome.lua
@@ -5,11 +5,15 @@ function serveressentials.show_sethome_help(pname)
 		"REAL MEN (and women who are REAL WOMEN) are competent to find the resources to craft a bed.",
 		"When you've learned to be self-reliant, you will never want to set eyes on /sethome or /home again.",
 		"The very thought of relying on govt' issued /sethome will fill you with disgust.",
+		"", -- Intentional empty line.
 	}
 
 	for _, line in ipairs(helplines) do
 		minetest.chat_send_player(pname, "# Server: " .. line)
 	end
+
+	minetest.chat_send_player(pname, "# Server: In the interest of being nominally helpful ...")
+	beds.report_respawn_status(pname)
 end
 
 if not serveressentials.sethome_registered then

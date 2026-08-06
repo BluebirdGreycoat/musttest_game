66b1df8042f9715854eeb45a9793173282baa97d
BlueBird51 <bluebird.greycoat@gmail.com>
2026-08-05 20:53:19 -0500

    Update

--- mods/camc/overlay.lua
+++ mods/camc/overlay.lua
@@ -3,9 +3,31 @@ camc.OVERLAYS = camc.OVERLAYS or {}
 
 function camc.setup_overlay(player)
 	local pname = player:get_player_name()
-	camc.OVERLAYS[pname] = {}
+	camc.OVERLAYS[pname] = camc.OVERLAYS[pname] or {}
 
 	local tab = camc.OVERLAYS[pname]
+
+	if tab.hud1 then
+		player:hud_remove(tab.hud1)
+		tab.hud1 = nil
+	end
+
+	local padding_x = 10   -- pixels from left edge
+	local padding_y = 10   -- pixels from bottom edge
+
+	local hud_id = player:hud_add({
+		type          = "text",
+		position      = {x = 0, y = 1},          -- bottom-left of screen
+		offset        = {x = padding_x, y = -padding_y},
+		text          = "Sample text",
+		alignment     = {x = -1, y = 1},         -- bottom-left corner of the text
+		number        = 0xFFFFFF,                -- white
+		scale         = {x = 100, y = 100},
+	})
+
+	if hud_id then
+		tab.hud1 = hud_id
+	end
 end
 
 function camc.hide_overlay(player)
@@ -30,7 +52,6 @@ function camc.remove_overlay(player)
 end
 
 function camc.update_overlay(player)
-	camc.system_response("MustTest", "Reloaded.")
 end
 
 function camc.on_mod_reload(modid)
@@ -40,6 +61,7 @@ function camc.on_mod_reload(modid)
 
 	local pref = minetest.get_player_by_name(camc.HAWKCAM_PLAYER)
 	if pref then
+		camc.setup_overlay(pref)
 		camc.update_overlay(pref)
 	end
 end

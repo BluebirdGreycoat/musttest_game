abfa9bf409941132e3e0610da97e94e92712135e
BlueBird51 <bluebird.greycoat@gmail.com>
2026-08-09 10:19:31 -0500

    Write mailbox db safely

--- mods/mailbox/storage.lua
+++ mods/mailbox/storage.lua
@@ -17,10 +17,7 @@ end
 mailbox.save = function()
   local str = minetest.serialize(mailbox.boxes)
   if type(str) ~= "string" then return end -- Failsafe.
+
   local path = minetest.get_worldpath() .. "/mailboxes.txt"
-  local file = io.open(path, "w")
-  if file then
-    file:write(str)
-    file:close()
-  end
+  minetest.safe_file_write(path, str)
 end
